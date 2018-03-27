d7b80d511549e5d453e5d2a9f2395a313d3c0606
Parachvte <ryannx6@gmail.com>
2018-03-27 12:17:14 +0800

    add warning for CNC feature

--- src/web/containers/App.jsx
+++ src/web/containers/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import shallowCompare from 'react-addons-shallow-compare';
 import { Redirect, withRouter } from 'react-router-dom';
+import modal from '../lib/modal';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Workspace from './Workspace';
@@ -9,14 +10,43 @@ import Cnc from './Cnc';
 import Settings from './Settings';
 import styles from './App.styl';
 
+
 class App extends Component {
     static propTypes = {
         ...withRouter.propTypes
     };
 
+    state = {
+        showCncWarning: false
+    };
+
     shouldComponentUpdate(nextProps, nextState) {
         return shallowCompare(this, nextProps, nextState);
     }
+
+    componentDidMount() {
+        const { history } = this.props;
+
+        history.listen(location => {
+            if (!this.state.showCncWarning && location.pathname === '/cnc') {
+                this.setState({ showCncWarning: true });
+                modal({
+                    title: 'Warning',
+                    body: (
+                        <div>
+                            <div>
+                                This is an alpha feature that helps you get started with CNC Engraving. Make sure
+                                you <a href="https://manual.snapmaker.com/cnc_carving/read_this_first_-_safety_information.html" target="_blank" rel="noopener noreferrer">
+                                Read This First - Safety Information</a>
+                                {' before any further instructions.'}
+                            </div>
+                        </div>
+                    )
+                });
+            }
+        });
+    }
+
     render() {
         const { location } = this.props;
         const accepted = ([
