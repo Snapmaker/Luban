93af3410635cc3b10cd253e94ade5eccb7475d13
Parachvte <ryannx6@gmail.com>
2018-03-31 16:28:10 +0800

    update package.json (fix windows build problem)

--- src/web/widgets/Axes/index.jsx
+++ src/web/widgets/Axes/index.jsx
@@ -252,15 +252,9 @@ class AxesWidget extends Component {
         },
         runBoundary: () => {
             const { bbox, workPosition } = this.state;
-            const headType = this.state.controller.state.headType;
 
             // absolute position
             controller.command('gcode', 'G90');
-
-            // go to safety height if z is too low
-            if (headType === 'CNC' && workPosition.z < 5) {
-                this.actions.ensureFeedrateCommand(`G0 Z${bbox.max.z}`);
-            }
             // run boundary
             this.actions.ensureFeedrateCommand(`G0 X${bbox.min.x} Y${bbox.min.y}`);
             this.actions.ensureFeedrateCommand(`G0 X${bbox.min.x} Y${bbox.max.y}`);
