e44e3b1c72bae340e8c5bf9c5dbb75d6c754398b
Parachvte <ryannx6@gmail.com>
2018-04-12 17:36:26 +0800

    copy for Workspace

--- src/web/widgets/Marlin/Overrides.jsx
+++ src/web/widgets/Marlin/Overrides.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import RepeatButton from '../../components/RepeatButton';
+import TipTrigger from '../../components/TipTrigger';
 import controller from '../../lib/controller';
 import DigitalReadout from './DigitalReadout';
 import styles from './index.styl';
@@ -15,66 +16,68 @@ const Overrides = (props) => {
     return (
         <div className={styles.overrides}>
             {!!ovF &&
-            <DigitalReadout label="F" value={ovF + '%'}>
-                <RepeatButton
-                    className="btn btn-default"
-                    style={{ padding: 5 }}
-                    onClick={() => {
-                        controller.command('feedOverride', -10);
-                    }}
-                >
-                    <i className="fa fa-arrow-down" style={{ fontSize: 14 }} />
-                    <span style={{ marginLeft: 5 }}>
-                        -10%
-                    </span>
-                </RepeatButton>
-                <RepeatButton
-                    className="btn btn-default"
-                    style={{ padding: 5 }}
-                    onClick={() => {
-                        controller.command('feedOverride', -1);
-                    }}
-                >
-                    <i className="fa fa-arrow-down" style={{ fontSize: 10 }} />
-                    <span style={{ marginLeft: 5 }}>
-                        -1%
-                    </span>
-                </RepeatButton>
-                <RepeatButton
-                    className="btn btn-default"
-                    style={{ padding: 5 }}
-                    onClick={() => {
-                        controller.command('feedOverride', 1);
-                    }}
-                >
-                    <i className="fa fa-arrow-up" style={{ fontSize: 10 }} />
-                    <span style={{ marginLeft: 5 }}>
-                        1%
-                    </span>
-                </RepeatButton>
-                <RepeatButton
-                    className="btn btn-default"
-                    style={{ padding: 5 }}
-                    onClick={() => {
-                        controller.command('feedOverride', 10);
-                    }}
-                >
-                    <i className="fa fa-arrow-up" style={{ fontSize: 14 }} />
-                    <span style={{ marginLeft: 5 }}>
-                        10%
-                    </span>
-                </RepeatButton>
-                <button
-                    type="button"
-                    className="btn btn-default"
-                    style={{ padding: 5 }}
-                    onClick={() => {
-                        controller.command('feedOverride', 0);
-                    }}
-                >
-                    <i className="fa fa-undo fa-fw" />
-                </button>
-            </DigitalReadout>
+            <TipTrigger content="Adjust feedrate percentage, which applies to moves along all axis.">
+                <DigitalReadout label="F" value={ovF + '%'}>
+                    <RepeatButton
+                        className="btn btn-default"
+                        style={{ padding: 5 }}
+                        onClick={() => {
+                            controller.command('feedOverride', -10);
+                        }}
+                    >
+                        <i className="fa fa-arrow-down" style={{ fontSize: 14 }} />
+                        <span style={{ marginLeft: 5 }}>
+                            -10%
+                        </span>
+                    </RepeatButton>
+                    <RepeatButton
+                        className="btn btn-default"
+                        style={{ padding: 5 }}
+                        onClick={() => {
+                            controller.command('feedOverride', -1);
+                        }}
+                    >
+                        <i className="fa fa-arrow-down" style={{ fontSize: 10 }} />
+                        <span style={{ marginLeft: 5 }}>
+                            -1%
+                        </span>
+                    </RepeatButton>
+                    <RepeatButton
+                        className="btn btn-default"
+                        style={{ padding: 5 }}
+                        onClick={() => {
+                            controller.command('feedOverride', 1);
+                        }}
+                    >
+                        <i className="fa fa-arrow-up" style={{ fontSize: 10 }} />
+                        <span style={{ marginLeft: 5 }}>
+                            1%
+                        </span>
+                    </RepeatButton>
+                    <RepeatButton
+                        className="btn btn-default"
+                        style={{ padding: 5 }}
+                        onClick={() => {
+                            controller.command('feedOverride', 10);
+                        }}
+                    >
+                        <i className="fa fa-arrow-up" style={{ fontSize: 14 }} />
+                        <span style={{ marginLeft: 5 }}>
+                            10%
+                        </span>
+                    </RepeatButton>
+                    <button
+                        type="button"
+                        className="btn btn-default"
+                        style={{ padding: 5 }}
+                        onClick={() => {
+                            controller.command('feedOverride', 0);
+                        }}
+                    >
+                        <i className="fa fa-undo fa-fw" />
+                    </button>
+                </DigitalReadout>
+            </TipTrigger>
             }
             {!!ovS && actions.is3DPrinting() &&
             <DigitalReadout label="S" value={ovS + '%'}>
