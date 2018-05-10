6ee3770ec8e75ca5329e0cc4b6e2a9c06e5c61ea
liuming <374406742@qq.com>
2018-05-10 11:35:07 +0800

    handle code review：https://github.com/whimsycwd/snapjs/pull/14#pullrequestreview-117803015

--- src/app/lib/Print3D-Slice.js
+++ src/app/lib/Print3D-Slice.js
@@ -2,15 +2,15 @@ import fs from 'fs';
 import logger from './logger';
 import { CURA_ENGINE_MAC, APP_CACHE_IMAGE } from '../constants';
 
-const log = logger('slice-progress');
+const log = logger('print3d-slice');
 
-var sliceProgress, filamentLength, filamentWeight, printTime;
+let sliceProgress, filamentLength, filamentWeight, printTime;
 function Print3DSlice(param, cb) {
-    var curaEnginePath = `${CURA_ENGINE_MAC}`;
-    var configPath = param.configFilePath;
-    var modelPath = `${APP_CACHE_IMAGE}/${param.modelFileName}`;
-    var gcodeFileName = 'output.gcode';
-    var gcodeFilePath = `${APP_CACHE_IMAGE}/${gcodeFileName}`;
+    let curaEnginePath = `${CURA_ENGINE_MAC}`;
+    let configPath = param.configFilePath;
+    let modelPath = `${APP_CACHE_IMAGE}/${param.modelFileName}`;
+    let gcodeFileName = 'output.gcode';
+    let gcodeFilePath = `${APP_CACHE_IMAGE}/${gcodeFileName}`;
     //check file exist
     if (!fs.existsSync(curaEnginePath)) {
         log.error('Slice Error : CuraEngine is not exist -> ' + curaEnginePath);
@@ -29,16 +29,15 @@ function Print3DSlice(param, cb) {
         cb('Slice Error : 3d model file is not exist -> ' + modelPath);
         return;
     }
-    var childProcess = require('child_process');
-    log.info('********************************');
+    let childProcess = require('child_process');
     log.info('call Cura Engine...');
     log.info('configPath = ' + configPath);
     log.info('gcodeFilePath = ' + gcodeFilePath);
     log.info('modelPath = ' + modelPath);
-    var wmic = childProcess.spawn(curaEnginePath, ['slice', '-v', '-p', '-j', configPath, '-o', gcodeFilePath, '-l', modelPath]);
+    let wmic = childProcess.spawn(curaEnginePath, ['slice', '-v', '-p', '-j', configPath, '-o', gcodeFilePath, '-l', modelPath]);
 
     wmic.stderr.on('data', (data) => {
-        var array = data.toString().split('\n');
+        let array = data.toString().split('\n');
 
         array.map((item) => {
             if (item.length < 10) {
@@ -46,8 +45,8 @@ function Print3DSlice(param, cb) {
             }
 
             if (item.indexOf('Progress:inset+skin:') === 0 || item.indexOf('Progress:export:') === 0) {
-                var start = item.indexOf('0.');
-                var end = item.indexOf('%');
+                let start = item.indexOf('0.');
+                let end = item.indexOf('%');
                 sliceProgress = Number(item.slice(start, end));
                 cb(undefined, sliceProgress);
             } else if (item.indexOf(';Filament used:') === 0) {
@@ -66,7 +65,7 @@ function Print3DSlice(param, cb) {
     });
 
     wmic.on('close', (code) => {
-        log.info('slice progress has closed **************************************');
+        log.info('slice progress closed with code ' + code);
     });
 }
 
