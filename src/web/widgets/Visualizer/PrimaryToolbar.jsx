6738a2eee2b797802bb4db566e59f04250e9fd11
Parachvte <ryannx6@gmail.com>
2018-05-28 17:13:49 +0800

    remove controller state form PrimaryToolbar

--- src/web/widgets/Visualizer/PrimaryToolbar.jsx
+++ src/web/widgets/Visualizer/PrimaryToolbar.jsx
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import classNames from 'classnames';
 import colornames from 'colornames';
 import React, { PureComponent } from 'react';
@@ -7,48 +6,7 @@ import { Dropdown, MenuItem } from 'react-bootstrap';
 import Detector from 'three/examples/js/Detector';
 import Interpolate from '../../components/Interpolate';
 import i18n from '../../lib/i18n';
-import {
-    // Grbl
-    GRBL,
-    GRBL_ACTIVE_STATE_IDLE,
-    GRBL_ACTIVE_STATE_RUN,
-    GRBL_ACTIVE_STATE_HOLD,
-    GRBL_ACTIVE_STATE_DOOR,
-    GRBL_ACTIVE_STATE_HOME,
-    GRBL_ACTIVE_STATE_SLEEP,
-    GRBL_ACTIVE_STATE_ALARM,
-    GRBL_ACTIVE_STATE_CHECK,
-    // Marlin
-    MARLIN,
-    // Smoothie
-    SMOOTHIE,
-    SMOOTHIE_ACTIVE_STATE_IDLE,
-    SMOOTHIE_ACTIVE_STATE_RUN,
-    SMOOTHIE_ACTIVE_STATE_HOLD,
-    SMOOTHIE_ACTIVE_STATE_DOOR,
-    SMOOTHIE_ACTIVE_STATE_HOME,
-    SMOOTHIE_ACTIVE_STATE_SLEEP,
-    SMOOTHIE_ACTIVE_STATE_ALARM,
-    SMOOTHIE_ACTIVE_STATE_CHECK,
-    // TinyG
-    TINYG,
-    TINYG_MACHINE_STATE_INITIALIZING,
-    TINYG_MACHINE_STATE_READY,
-    TINYG_MACHINE_STATE_ALARM,
-    TINYG_MACHINE_STATE_STOP,
-    TINYG_MACHINE_STATE_END,
-    TINYG_MACHINE_STATE_RUN,
-    TINYG_MACHINE_STATE_HOLD,
-    TINYG_MACHINE_STATE_PROBE,
-    TINYG_MACHINE_STATE_CYCLE,
-    TINYG_MACHINE_STATE_HOMING,
-    TINYG_MACHINE_STATE_JOG,
-    TINYG_MACHINE_STATE_INTERLOCK,
-    TINYG_MACHINE_STATE_SHUTDOWN,
-    TINYG_MACHINE_STATE_PANIC,
-    // Workflow
-    WORKFLOW_STATE_IDLE
-} from '../../constants';
+import { WORKFLOW_STATE_IDLE } from '../../constants';
 import styles from './primary-toolbar.styl';
 
 class PrimaryToolbar extends PureComponent {
@@ -84,107 +42,8 @@ class PrimaryToolbar extends PureComponent {
         );
     }
     renderControllerState() {
-        const { state } = this.props;
-        const controllerType = state.controller.type;
-        const controllerState = state.controller.state;
         let stateStyle = '';
         let stateText = '';
-
-        if (controllerType === GRBL) {
-            const activeState = _.get(controllerState, 'status.activeState');
-
-            stateStyle = {
-                [GRBL_ACTIVE_STATE_IDLE]: 'controller-state-default',
-                [GRBL_ACTIVE_STATE_RUN]: 'controller-state-primary',
-                [GRBL_ACTIVE_STATE_HOLD]: 'controller-state-warning',
-                [GRBL_ACTIVE_STATE_DOOR]: 'controller-state-warning',
-                [GRBL_ACTIVE_STATE_HOME]: 'controller-state-primary',
-                [GRBL_ACTIVE_STATE_SLEEP]: 'controller-state-success',
-                [GRBL_ACTIVE_STATE_ALARM]: 'controller-state-danger',
-                [GRBL_ACTIVE_STATE_CHECK]: 'controller-state-info'
-            }[activeState];
-
-            stateText = {
-                [GRBL_ACTIVE_STATE_IDLE]: i18n.t('controller:Grbl.activeState.idle'),
-                [GRBL_ACTIVE_STATE_RUN]: i18n.t('controller:Grbl.activeState.run'),
-                [GRBL_ACTIVE_STATE_HOLD]: i18n.t('controller:Grbl.activeState.hold'),
-                [GRBL_ACTIVE_STATE_DOOR]: i18n.t('controller:Grbl.activeState.door'),
-                [GRBL_ACTIVE_STATE_HOME]: i18n.t('controller:Grbl.activeState.home'),
-                [GRBL_ACTIVE_STATE_SLEEP]: i18n.t('controller:Grbl.activeState.sleep'),
-                [GRBL_ACTIVE_STATE_ALARM]: i18n.t('controller:Grbl.activeState.alarm'),
-                [GRBL_ACTIVE_STATE_CHECK]: i18n.t('controller:Grbl.activeState.check')
-            }[activeState];
-        }
-
-        // FIXME
-        if (controllerType === MARLIN) {
-            // Unsupported
-        }
-
-        if (controllerType === SMOOTHIE) {
-            const activeState = _.get(controllerState, 'status.activeState');
-
-            stateStyle = {
-                [SMOOTHIE_ACTIVE_STATE_IDLE]: 'controller-state-default',
-                [SMOOTHIE_ACTIVE_STATE_RUN]: 'controller-state-primary',
-                [SMOOTHIE_ACTIVE_STATE_HOLD]: 'controller-state-warning',
-                [SMOOTHIE_ACTIVE_STATE_DOOR]: 'controller-state-warning',
-                [SMOOTHIE_ACTIVE_STATE_HOME]: 'controller-state-primary',
-                [SMOOTHIE_ACTIVE_STATE_SLEEP]: 'controller-state-success',
-                [SMOOTHIE_ACTIVE_STATE_ALARM]: 'controller-state-danger',
-                [SMOOTHIE_ACTIVE_STATE_CHECK]: 'controller-state-info'
-            }[activeState];
-
-            stateText = {
-                [SMOOTHIE_ACTIVE_STATE_IDLE]: i18n.t('controller:Smoothie.activeState.idle'),
-                [SMOOTHIE_ACTIVE_STATE_RUN]: i18n.t('controller:Smoothie.activeState.run'),
-                [SMOOTHIE_ACTIVE_STATE_HOLD]: i18n.t('controller:Smoothie.activeState.hold'),
-                [SMOOTHIE_ACTIVE_STATE_DOOR]: i18n.t('controller:Smoothie.activeState.door'),
-                [SMOOTHIE_ACTIVE_STATE_HOME]: i18n.t('controller:Smoothie.activeState.home'),
-                [SMOOTHIE_ACTIVE_STATE_SLEEP]: i18n.t('controller:Smoothie.activeState.sleep'),
-                [SMOOTHIE_ACTIVE_STATE_ALARM]: i18n.t('controller:Smoothie.activeState.alarm'),
-                [SMOOTHIE_ACTIVE_STATE_CHECK]: i18n.t('controller:Smoothie.activeState.check')
-            }[activeState];
-        }
-
-        if (controllerType === TINYG) {
-            const machineState = _.get(controllerState, 'sr.machineState');
-
-            // https://github.com/synthetos/g2/wiki/Alarm-Processing
-            stateStyle = {
-                [TINYG_MACHINE_STATE_INITIALIZING]: 'controller-state-warning',
-                [TINYG_MACHINE_STATE_READY]: 'controller-state-default',
-                [TINYG_MACHINE_STATE_ALARM]: 'controller-state-danger',
-                [TINYG_MACHINE_STATE_STOP]: 'controller-state-default',
-                [TINYG_MACHINE_STATE_END]: 'controller-state-default',
-                [TINYG_MACHINE_STATE_RUN]: 'controller-state-primary',
-                [TINYG_MACHINE_STATE_HOLD]: 'controller-state-warning',
-                [TINYG_MACHINE_STATE_PROBE]: 'controller-state-primary',
-                [TINYG_MACHINE_STATE_CYCLE]: 'controller-state-primary',
-                [TINYG_MACHINE_STATE_HOMING]: 'controller-state-primary',
-                [TINYG_MACHINE_STATE_JOG]: 'controller-state-primary',
-                [TINYG_MACHINE_STATE_INTERLOCK]: 'controller-state-warning',
-                [TINYG_MACHINE_STATE_SHUTDOWN]: 'controller-state-danger',
-                [TINYG_MACHINE_STATE_PANIC]: 'controller-state-danger'
-            }[machineState];
-
-            stateText = {
-                [TINYG_MACHINE_STATE_INITIALIZING]: i18n.t('controller:TinyG.machineState.initializing'),
-                [TINYG_MACHINE_STATE_READY]: i18n.t('controller:TinyG.machineState.ready'),
-                [TINYG_MACHINE_STATE_ALARM]: i18n.t('controller:TinyG.machineState.alarm'),
-                [TINYG_MACHINE_STATE_STOP]: i18n.t('controller:TinyG.machineState.stop'),
-                [TINYG_MACHINE_STATE_END]: i18n.t('controller:TinyG.machineState.end'),
-                [TINYG_MACHINE_STATE_RUN]: i18n.t('controller:TinyG.machineState.run'),
-                [TINYG_MACHINE_STATE_HOLD]: i18n.t('controller:TinyG.machineState.hold'),
-                [TINYG_MACHINE_STATE_PROBE]: i18n.t('controller:TinyG.machineState.probe'),
-                [TINYG_MACHINE_STATE_CYCLE]: i18n.t('controller:TinyG.machineState.cycle'),
-                [TINYG_MACHINE_STATE_HOMING]: i18n.t('controller:TinyG.machineState.homing'),
-                [TINYG_MACHINE_STATE_JOG]: i18n.t('controller:TinyG.machineState.jog'),
-                [TINYG_MACHINE_STATE_INTERLOCK]: i18n.t('controller:TinyG.machineState.interlock'),
-                [TINYG_MACHINE_STATE_SHUTDOWN]: i18n.t('controller:TinyG.machineState.shutdown'),
-                [TINYG_MACHINE_STATE_PANIC]: i18n.t('controller:TinyG.machineState.panic')
-            }[machineState];
-        }
 
         return (
             <div
@@ -199,21 +58,7 @@ class PrimaryToolbar extends PureComponent {
     }
     getWorkCoordinateSystem() {
         const { state } = this.props;
-        const controllerType = state.controller.type;
-        const controllerState = state.controller.state;
         const defaultWCS = 'G54';
-
-        if (controllerType === GRBL) {
-            return _.get(controllerState, 'parserstate.modal.coordinate') || defaultWCS;
-        }
-
-        if (controllerType === SMOOTHIE) {
-            return _.get(controllerState, 'parserstate.modal.coordinate') || defaultWCS;
-        }
-
-        if (controllerType === TINYG) {
-            return _.get(controllerState, 'sr.modal.coordinate') || defaultWCS;
-        }
 
         return defaultWCS;
     }
