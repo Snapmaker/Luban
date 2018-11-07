4494508906b627fb180c4d11f73892b00cc71e68
liumingzw <374406742@qq.com>
2018-11-07 16:25:15 +0800

    3dp feature lay flat model (#46)
    - Added convex hull geometry for Model
    - Optimization: Reduced compute time of bbox by using convex hull geometry rather than model geometry
    -  New feature: Lay flat 3DP model

--- src/web/widgets/ThreeDPrintingVisualizer/ContextMenu.js
+++ src/web/widgets/ThreeDPrintingVisualizer/ContextMenu.js
@@ -6,44 +6,41 @@ import i18n from '../../lib/i18n';
 import styles from './styles.styl';
 import { STAGES_3DP } from '../../constants';
 
+
 class ContextMenu extends PureComponent {
     static propTypes = {
         actions: PropTypes.object.isRequired,
         state: PropTypes.object.isRequired
     };
+
     render() {
-        const actions = this.props.actions;
-        const state = this.props.state;
+        const { actions, state } = this.props;
         const isModelSelected = !!state.selectedModel;
         const isModelLoaded = (state.stage === STAGES_3DP.modelLoaded);
         return (
             <React.Fragment>
                 <Anchor
                     className={classNames(
-                        styles['contextMenu--option'],
-                        isModelSelected ? '' : styles['contextMenu--option__disabled']
+                        styles['context-menu__option'],
+                        isModelSelected ? '' : styles['context-menu__option--disabled']
                     )}
-                    onClick={() => {
-                        actions.centerSelectedModel();
-                    }}
+                    onClick={actions.centerSelectedModel}
                 >
                     {i18n._('Center Selected Model')}
                 </Anchor>
                 <Anchor
                     className={classNames(
-                        styles['contextMenu--option'],
-                        isModelSelected ? '' : styles['contextMenu--option__disabled']
+                        styles['context-menu__option'],
+                        isModelSelected ? '' : styles['context-menu__option--disabled']
                     )}
-                    onClick={() => {
-                        actions.deleteSelectedModel();
-                    }}
+                    onClick={actions.deleteSelectedModel}
                 >
                     {i18n._('Delete Selected Model')}
                 </Anchor>
                 <Anchor
                     className={classNames(
-                        styles['contextMenu--option'],
-                        isModelSelected ? '' : styles['contextMenu--option__disabled']
+                        styles['context-menu__option'],
+                        isModelSelected ? '' : styles['context-menu__option--disabled']
                     )}
                     onClick={() => {
                         actions.multiplySelectedModel(1);
@@ -53,37 +50,40 @@ class ContextMenu extends PureComponent {
                 </Anchor>
                 <Anchor
                     className={classNames(
-                        styles['contextMenu--option'],
-                        isModelSelected ? '' : styles['contextMenu--option__disabled']
+                        styles['context-menu__option'],
+                        isModelSelected ? '' : styles['context-menu__option--disabled']
                     )}
-                    onClick={() => {
-                        actions.resetSelectedModelTransformation();
-                    }}
+                    onClick={actions.resetSelectedModelTransformation}
                 >
                     {i18n._('Reset Selected Model Transformation')}
                 </Anchor>
+                <Anchor
+                    className={classNames(
+                        styles['context-menu__option'],
+                        isModelSelected ? '' : styles['context-menu__option--disabled']
+                    )}
+                    onClick={actions.layFlatSelectedModel}
+                >
+                    {i18n._('Lay Flat Selected Model')}
+                </Anchor>
                 <div
-                    className={classNames(styles['contextMenu--separator'])}
+                    className={classNames(styles['context-menu__separator'])}
                 />
                 <Anchor
                     className={classNames(
-                        styles['contextMenu--option'],
-                        isModelLoaded ? '' : styles['contextMenu--option__disabled']
+                        styles['context-menu__option'],
+                        isModelLoaded ? '' : styles['context-menu__option--disabled']
                     )}
-                    onClick={() => {
-                        actions.clearBuildPlate();
-                    }}
+                    onClick={actions.clearBuildPlate}
                 >
                     {i18n._('Clear Build Plate')}
                 </Anchor>
                 <Anchor
                     className={classNames(
-                        styles['contextMenu--option'],
-                        isModelLoaded ? '' : styles['contextMenu--option__disabled']
+                        styles['context-menu__option'],
+                        isModelLoaded ? '' : styles['context-menu__option--disabled']
                     )}
-                    onClick={() => {
-                        actions.arrangeAllModels();
-                    }}
+                    onClick={actions.arrangeAllModels}
                 >
                     {i18n._('Arrange All Models')}
                 </Anchor>
--- src/web/widgets/ThreeDPrintingVisualizer/Model.js
+++ src/web/widgets/ThreeDPrintingVisualizer/Model.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import ConvexGeometry from '../../components/three-extensions/ConvexGeometry';
+
 
 class Model extends THREE.Mesh {
     constructor(geometry, materialNormal, materialOverstepped, modelPath) {
@@ -20,6 +22,30 @@ class Model extends THREE.Mesh {
         this.boundingBox = null;
         this._selected = false;
         this.setSelected(this._selected);
+
+        // convex hull geometry
+        // attention: it may cost several seconds
+        // why use ConvexGeometry rather than ConvexBufferGeometry?
+        // ConvexGeometry will execute "mergeVertices()" to remove duplicated vertices
+        // no performance different if not render
+        this.convexGeometry = null;
+        if (geometry.isBufferGeometry) {
+            const tempGeometry = new THREE.Geometry();
+            tempGeometry.fromBufferGeometry(geometry);
+            this.convexGeometry = new ConvexGeometry(tempGeometry.vertices);
+        } else {
+            this.convexGeometry = new ConvexGeometry(geometry.vertices);
+        }
+
+        // render convex hull
+        // const meshMaterial = new THREE.MeshLambertMaterial({
+        //     color: 0xffffff,
+        //     opacity: 0.8,
+        //     transparent: true
+        // });
+        // const convexHullMesh = new THREE.Mesh(this.convexGeometry, meshMaterial);
+        // convexHullMesh.scale.set(1, 1, 1);
+        // this.add(convexHullMesh);
     }
 
     alignWithParent() {
@@ -33,11 +59,12 @@ class Model extends THREE.Mesh {
         // after operated(move/scale/rotate), model.geometry is not changed
         // so need to call: bufferGemotry.applyMatrix(matrixLocal);
         // then call: bufferGemotry.computeBoundingBox(); to get operated modelMesh BoundingBox
+        // clone this.convexGeometry then clone.computeBoundingBox() is faster.
+        const clone = this.convexGeometry.clone();
         this.updateMatrix();
-        const bufferGemotry = this.geometry.clone();
-        bufferGemotry.applyMatrix(this.matrix);
-        bufferGemotry.computeBoundingBox();
-        this.boundingBox = bufferGemotry.boundingBox;
+        clone.applyMatrix(this.matrix);
+        clone.computeBoundingBox();
+        this.boundingBox = clone.boundingBox;
     }
 
     setSelected(selected) {
@@ -75,6 +102,136 @@ class Model extends THREE.Mesh {
         );
         clone.setMatrix(this.matrix);
         return clone;
+    }
+
+    layFlat() {
+        const epsilon = 1e-6;
+        const positionX = this.position.x;
+        const positionZ = this.position.z;
+
+        // Attention: the minY-vertex and min-angle-vertex must be in the same face
+        // transform convexGeometry clone
+        this.updateMatrix();
+        let convexGeometryClone = this.convexGeometry.clone();
+        convexGeometryClone.applyMatrix(this.matrix);
+        let faces = convexGeometryClone.faces;
+        let vertices = convexGeometryClone.vertices;
+
+        // find out the following params:
+        let minY = Number.MAX_VALUE;
+        let minYVertexIndex = -1;
+        let minAngleVertexIndex = -1; // The angle between the vector(minY-vertex -> min-angle-vertex) and the x-z plane is minimal
+        let minAngleFace = null;
+
+        // find minY and minYVertexIndex
+        for (let i = 0; i < vertices.length; i++) {
+            if (vertices[i].y < minY) {
+                minY = vertices[i].y;
+                minYVertexIndex = i;
+            }
+        }
+
+        // get minY vertices count
+        let minYVerticesCount = 0;
+        for (let i = 0; i < vertices.length; i++) {
+            if (vertices[i].y - minY < epsilon) {
+                ++minYVerticesCount;
+            }
+        }
+
+        if (minYVerticesCount >= 3) {
+            // already lay flat
+            return;
+        }
+
+        // find minAngleVertexIndex
+        if (minYVerticesCount === 2) {
+            for (let i = 0; i < vertices.length; i++) {
+                if (vertices[i].y - minY < epsilon && i !== minYVertexIndex) {
+                    minAngleVertexIndex = i;
+                }
+            }
+        } else if (minYVerticesCount === 1) {
+            let sinValue = Number.MAX_VALUE; // sin value of the angle between directionVector3 and x-z plane
+            for (let i = 1; i < vertices.length; i++) {
+                if (i !== minYVertexIndex) {
+                    const directionVector3 = new THREE.Vector3().subVectors(vertices[i], vertices[minYVertexIndex]);
+                    const length = directionVector3.length();
+                    // min sinValue corresponds minAngleVertexIndex
+                    if (directionVector3.y / length < sinValue) {
+                        sinValue = directionVector3.y / length;
+                        minAngleVertexIndex = i;
+                    }
+                }
+            }
+            // transform model to make min-angle-vertex y equal to minY
+            const vb1 = new THREE.Vector3().subVectors(vertices[minAngleVertexIndex], vertices[minYVertexIndex]);
+            const va1 = new THREE.Vector3(vb1.x, 0, vb1.z);
+            const matrix1 = this._getRotateMatrix(va1, vb1);
+            this.applyMatrix(matrix1);
+            this.alignWithParent();
+
+            // update geometry
+            convexGeometryClone = this.convexGeometry.clone();
+            convexGeometryClone.applyMatrix(this.matrix);
+            faces = convexGeometryClone.faces;
+        }
+
+        // now there must be 2 minY vertices
+        // find minAngleFace
+        const candidateFaces = [];
+        for (let i = 0; i < faces.length; i++) {
+            const face = faces[i];
+            if ([face.a, face.b, face.c].includes(minYVertexIndex) &&
+                [face.a, face.b, face.c].includes(minAngleVertexIndex)) {
+                candidateFaces.push(face);
+            }
+        }
+
+        // max cos value corresponds min angle
+        convexGeometryClone.computeFaceNormals();
+        let cosValue = Number.MIN_VALUE;
+        for (let i = 0; i < candidateFaces.length; i++) {
+            // faceNormal points model outer surface
+            const faceNormal = candidateFaces[i].normal;
+            if (faceNormal.y < 0) {
+                const cos = -faceNormal.y / faceNormal.length();
+                if (cos > cosValue) {
+                    cosValue = cos;
+                    minAngleFace = candidateFaces[i];
+                }
+            }
+        }
+
+        const xzPlaneNormal = new THREE.Vector3(0, -1, 0);
+        const vb2 = minAngleFace.normal;
+        const matrix2 = this._getRotateMatrix(xzPlaneNormal, vb2);
+        this.applyMatrix(matrix2);
+        this.alignWithParent();
+        this.position.x = positionX;
+        this.position.z = positionZ;
+    }
+
+    // get matrix for rotating v2 to v1. Applying matrix to v2 can make v2 to parallels v1.
+    _getRotateMatrix(v1, v2) {
+        // https://stackoverflow.com/questions/1171849/finding-quaternion-representing-the-rotation-from-one-vector-to-another
+        const cross = new THREE.Vector3();
+        cross.crossVectors(v2, v1);
+        const dot = v1.dot(v2);
+
+        const l1 = v1.length();
+        const l2 = v2.length();
+        const w = l1 * l2 + dot;
+        const x = cross.x;
+        const y = cross.y;
+        const z = cross.z;
+
+        const q = new THREE.Quaternion(x, y, z, w);
+        q.normalize();
+
+        const matrix4 = new THREE.Matrix4();
+        matrix4.makeRotationFromQuaternion(q);
+        return matrix4;
     }
 }
 
