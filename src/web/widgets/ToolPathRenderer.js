2afb3d7add75f0ae49237e4d65d705913d58bb8a
parachvte <ryannx6@gmail.com>
2018-12-01 03:26:15 +0800

    Visual: Make G0 invisible in laser & CNC tool path preview (#55)

--- src/web/widgets/ToolPathRenderer.js
+++ src/web/widgets/ToolPathRenderer.js
@@ -1,5 +1,30 @@
 import * as THREE from 'three';
 
+const UNIFORMS = {
+    // rgba
+    u_g1_color: new THREE.Uniform(new THREE.Vector4(0, 0, 0, 1))
+};
+
+const CNC_LASER_VERT_SHADER = [
+    'varying float v_g_code;',
+    'attribute float a_g_code;',
+    'void main(){',
+    '    v_g_code = a_g_code;',
+    '    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);',
+    '}'
+].join('');
+
+const CNC_LASER_FRAG_SHADER = [
+    'uniform vec4 u_g1_color;',
+    'varying float v_g_code;',
+    'void main(){',
+    '    if(v_g_code == 0.0){',
+    '        discard;',
+    '    }',
+    '    gl_FragColor = u_g1_color;',
+    '}'
+].join('');
+
 const motionColor = {
     'G0': new THREE.Color(0xc8c8c8),
     'G1': new THREE.Color(0x000000),
@@ -24,13 +49,9 @@ class ToolPathRenderer {
     }
 
     _parseToLine(data) {
-        const geometry = new THREE.Geometry();
-        const material = new THREE.LineBasicMaterial({
-            linewidth: 1,
-            vertexColors: THREE.VertexColors,
-            opacity: 1,
-            transparent: true
-        });
+        const positions = [];
+        const gCodes = [];
+
         let state = {
             G: 0,
             X: 0,
@@ -45,16 +66,11 @@ class ToolPathRenderer {
             item.Y !== undefined && (newState.Y = item.Y);
             item.Z !== undefined && (newState.Z = item.Z);
 
-            if (state.G !== newState.G) {
-                // G changed, add data of pre vertex of current color
-                geometry.vertices.push(new THREE.Vector3(state.X, state.Y, state.Z));
-                if (newState.G === 0) {
-                    geometry.colors.push(motionColor.G0);
-                } else if (newState.G === 1) {
-                    geometry.colors.push(motionColor.G1);
-                } else {
-                    geometry.colors.push(motionColor.unknown);
-                }
+            if ((state.G === 1) && (newState.G === 0)) {
+                positions.push(state.X);
+                positions.push(state.Y);
+                positions.push(state.Z);
+                gCodes.push(newState.G);
             }
 
             if (state.G !== newState.G ||
@@ -62,17 +78,27 @@ class ToolPathRenderer {
                 state.Y !== newState.Y ||
                 state.Z !== newState.Z) {
                 state = newState;
-                geometry.vertices.push(new THREE.Vector3(state.X, state.Y, state.Z));
-                if (state.G === 0) {
-                    geometry.colors.push(motionColor.G0);
-                } else if (state.G === 1) {
-                    geometry.colors.push(motionColor.G1);
-                } else {
-                    geometry.colors.push(motionColor.unknown);
-                }
+                positions.push(state.X);
+                positions.push(state.Y);
+                positions.push(state.Z);
+                gCodes.push(state.G);
             }
         }
-        return new THREE.Line(geometry, material);
+
+        const bufferGeometry = new THREE.BufferGeometry();
+        const positionAttribute = new THREE.Float32BufferAttribute(positions, 3);
+        const gCodeAttribute = new THREE.Float32BufferAttribute(gCodes, 1);
+        bufferGeometry.addAttribute('position', positionAttribute);
+        bufferGeometry.addAttribute('a_g_code', gCodeAttribute);
+        const material = new THREE.ShaderMaterial({
+            uniforms: UNIFORMS,
+            vertexShader: CNC_LASER_VERT_SHADER,
+            fragmentShader: CNC_LASER_FRAG_SHADER,
+            side: THREE.DoubleSide,
+            transparent: true,
+            linewidth: 1
+        });
+        return new THREE.Line(bufferGeometry, material);
     }
 
     _parseToPoints(data) {
