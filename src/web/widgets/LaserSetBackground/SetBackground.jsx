7cf44eecc9b552c8f871e33292ab5279d253ec5c
parachvte <ryannx6@gmail.com>
2019-01-28 15:03:12 +0800

    Refactor: Moved instructions to a individual Component

--- src/web/widgets/LaserSetBackground/SetBackground.jsx
+++ src/web/widgets/LaserSetBackground/SetBackground.jsx
@@ -4,14 +4,13 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import i18n from '../../lib/i18n';
-import { WORKFLOW_STATE_IDLE, BOUND_SIZE } from '../../constants';
+import { BOUND_SIZE } from '../../constants';
 import Modal from '../../components/Modal';
-import Space from '../../components/Space/Space';
-import controller from '../../lib/controller';
 import styles from './styles.styl';
 import modal from '../../lib/modal';
 import PrintBgImg from './PrintBgImg';
 import ExtractBgImg from './ExtractBgImg';
+import Instructions from './Instructions';
 import { actions } from '../../reducers/modules/laser';
 
 
@@ -45,27 +44,16 @@ class SetBackground extends PureComponent {
         },
         checkConnectionStatus: () => {
             const { isConnected, isLaser } = this.props.state;
-            const isIdle = controller.workflowState === WORKFLOW_STATE_IDLE;
-            if (isConnected && isLaser && isIdle) {
+
+            if (isConnected && isLaser) {
                 return true;
-            } else {
-                // alert
-                let msg = '';
-                if (!isIdle) {
-                    msg = 'Please wait for idle.';
-                }
-                if (!isLaser) {
-                    msg = 'Please change to laser head.';
-                }
-                if (!isConnected) {
-                    msg = 'Please connect first.';
-                }
-                modal({
-                    title: i18n._('Can not operate'),
-                    body: i18n._(`${msg}`)
-                });
-                return false;
             }
+
+            modal({
+                title: i18n._('Warning'),
+                body: i18n._('Please make sure laser tool head is installed properly, and then connect the machine via Connection widget.')
+            });
+            return false;
         },
         showBgImgSettingModal: () => {
             this.setState({ showBgImgSettingModal: true });
@@ -83,10 +71,9 @@ class SetBackground extends PureComponent {
             const { sideLength, bgImgFilename } = this.state;
 
             if (!bgImgFilename) {
-                const msg = 'Please extract background image from photo.';
                 modal({
-                    title: i18n._('Can not operate'),
-                    body: i18n._(`${msg}`)
+                    title: i18n._('Information'),
+                    body: i18n._('Please extract background image from photo.')
                 });
                 return;
             }
@@ -106,74 +93,7 @@ class SetBackground extends PureComponent {
 
         return (
             <React.Fragment>
-                {state.showInstructions &&
-                <Modal style={{ width: '1080px' }} size="lg" onClose={actions.hideInstructions}>
-                    <Modal.Header>
-                        <Modal.Title>
-                            {i18n._('How Fine Tune Work Origin Works')}
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body className={styles['test-laser-instruction-content']}>
-                        <p>{i18n._('Setting work origin is essentially finding the best place for the engraved image \
-in the X and Y directions and determining the distance (Z Offset) between the Engraving & Carving Platform and the \
-Laser Module to acquire the smallest laser dot on the material for the most efficient use of the laser power and the \
-best result. For the 200mW Laser Engraving Module, the Z Offset can be set by judging the size of the laser dot by eyes \
-with low power. However, for the 1600mW Laser Cutting Module, this method is less accurate as the laser dot is too \
-strong and less interpretable. To set the Z Offset more accurately, we can move the module to the position that is \
-close to the optimal Z Offset (Offset A). The software will test the results from a few positions next to Offset A \
-on the same material. The best result determines the best Z Offset.')}
-                        </p>
-                        <div className={styles['test-laser-instruction-step']}>
-                            <img
-                                src="images/laser/laser-test-instructions-01.png"
-                                role="presentation"
-                                alt="x"
-                            />
-                            <p>
-                                <span>{i18n._('Click')}</span>
-                                <span style={{ color: '#28a7e1', padding: '0 4px' }}>{i18n._('Focus')}</span>
-                                <span>{i18n._('and use')}</span>
-                                <span style={{ color: '#28a7e1', padding: '0 4px' }}>{i18n._('Jog Pad')}</span>
-                                <span>{i18n._('in the Axes section to move the Laser Cutting Module to the position \
-that is close to the optimal Z Offset (just like how you do with the 200mW Laser Engraving Module).')}
-                                </span>
-                            </p>
-                        </div>
-                        <div className={styles['test-laser-instruction-step']}>
-                            <img
-                                src="images/laser/laser-test-instructions-02.png"
-                                role="presentation"
-                                alt="x"
-                            />
-                            <p>
-                                <span>{i18n._('Set Work Speed and Power based on the material you are using. If you are using \
-a piece of 1.5 mm wood sheet, it’s recommended to set the Work Speed to a value between 80 mm/s and 120 mm/s and set the Power to 100%.')}
-                                </span>
-                                <Space width={4} />
-                                <span>{i18n._('Click')}</span>
-                                <span style={{ color: '#28a7e1', padding: '0 4px' }}>{i18n._('Generate and Load G-code')}</span>
-                                <span>{i18n._('and the G-code is automatically generated and loaded.')}</span>
-                            </p>
-                        </div>
-                        <div className={styles['test-laser-instruction-step']}>
-                            <img
-                                src="images/laser/laser-test-instructions-03.png"
-                                role="presentation"
-                                alt="x"
-                            />
-                            <p>
-                                <span>{i18n._('Click')}</span>
-                                <span className="fa fa-play" style={{ padding: '0 4px' }} />
-                                <span>{i18n._('to start laser cutting.')}</span>
-                                <Space width={4} />
-                                <span>{i18n._('Choose the position that can cut the material the most smoothly or \
-engrave the thinnest line and the software will set it as Z Offset. In this example, -2.0 should be the Z Offset.')}
-                                </span>
-                            </p>
-                        </div>
-                    </Modal.Body>
-                </Modal>
-                }
+                {state.showInstructions && <Instructions actions={actions} />}
                 {state.showBgImgSettingModal &&
                 <Modal style={{ width: '900px', height: '700px' }} size="lg" onClose={actions.hideBgImgSettingModal}>
                     <Modal.Header>
