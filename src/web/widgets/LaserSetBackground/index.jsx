79d95da68dcdd79b35f0fcaec539ada3a43975b4
liumingzw <374406742@qq.com>
2019-02-14 18:36:50 +0800

    delete unused  bracket

--- src/web/widgets/LaserSetBackground/index.jsx
+++ src/web/widgets/LaserSetBackground/index.jsx
@@ -82,11 +82,9 @@ class LaserSetBackgroundWidget extends PureComponent {
                         {i18n._('Set Laser Background')}
                     </Widget.Title>
                     <Widget.Controls className="sortable-filter">
-                        {
-                            <Widget.Button onClick={this.actions.showInstructions} >
-                                <i className="fa fa-info" />
-                            </Widget.Button>
-                        }
+                        <Widget.Button onClick={this.actions.showInstructions} >
+                            <i className="fa fa-info" />
+                        </Widget.Button>
                         <DefaultMinimizeButton widgetState={widgetState} />
                         <DefaultDropdownButton widgetState={widgetState} />
                     </Widget.Controls>
