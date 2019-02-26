6dfe691eabd4540dea324ab876fab30f95ebd9f2
liumingzw <374406742@qq.com>
2019-02-26 11:02:09 +0800

    add module react-contextify and package ContextMenu component

---
+++ src/web/components/ContextMenu/index.jsx
@@ -0,0 +1,58 @@
+import React, { PureComponent } from 'react';
+import { Menu, Item, Separator, contextMenu } from 'react-contexify';
+import 'react-contexify/dist/ReactContexify.min.css';
+import PropTypes from 'prop-types';
+
+
+// ref: https://fkhadra.github.io/react-contexify/api/context-menu
+class ContextMenu extends PureComponent {
+    show(e) {
+        e.preventDefault();
+        contextMenu.show({
+            id: this.props.id,
+            event: e
+        });
+    }
+    static hide() {
+        contextMenu.hideAll();
+    }
+    render() {
+        const { id, items = [] } = this.props;
+        let key = 0;
+        return (
+            <div>
+                <Menu id={id}>
+                    {items.map((item) => {
+                        if (!item) {
+                            return null;
+                        }
+                        if (item === 'separator') {
+                            return (
+                                <Separator key={key++} />
+                            );
+                        } else {
+                            const { str, disabled = false, onClick } = item;
+                            return (
+                                <Item key={key++} onClick={onClick} disabled={disabled}>{str}</Item>
+                            );
+                        }
+                    })}
+                </Menu>
+            </div>
+        );
+    }
+}
+
+ContextMenu.propTypes = {
+    id: PropTypes.string.isRequired,
+    // example:
+    //     items = [
+    //         { str: 'a-1', disabled: false, onClick: () => console.log('a-1') },
+    //         'separator',
+    //         { str: 'a-2', disabled: true, onClick: () => console.log('a-2') },
+    //         { str: 'a-3', disabled: false, onClick: () => console.log('a-3') }
+    //     ];
+    items: PropTypes.array.isRequired
+};
+
+export default ContextMenu;
