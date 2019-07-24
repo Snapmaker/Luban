20d0706a724c2f19e0990f120cdecc0cfe53bf9b
parachvte <ryannx6@gmail.com>
2019-07-24 19:58:16 +0800

    Remove Panel component

--- src/app/components/Panel/index.js
+++
@@ -1,8 +0,0 @@
-import Panel from './Panel';
-import PanelHeading from './PanelHeading';
-import PanelBody from './PanelBody';
-
-Panel.Heading = PanelHeading;
-Panel.Body = PanelBody;
-
-export default Panel;
