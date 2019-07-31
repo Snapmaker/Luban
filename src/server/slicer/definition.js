de6959b1921a4b418afb9d2695c72c5e6fd7738f
zhangjiefeng <zjief1992@gmail.com>
2019-07-31 16:13:28 +0800

    BUGFIX: new config cant not update when v2.5.5 to v2.6.0

--- src/server/slicer/definition.js
+++ src/server/slicer/definition.js
@@ -3,7 +3,6 @@ import path from 'path';
 import includes from 'lodash/includes';
 import DataStorage from '../DataStorage';
 
-
 const SETTING_FIELDS = [
     'label', 'description', 'type', 'options', 'unit', 'enabled', 'default_value', 'value',
     // Snapmaker extended fields:
@@ -136,27 +135,39 @@ export class DefinitionLoader {
     }
 }
 
+function writeDefinition(filename, definitionLoader) {
+    const filePath = path.join(DataStorage.configDir, filename);
+    fs.writeFile(filePath, JSON.stringify(definitionLoader.toJSON(), null, 2), 'utf8', (err) => {
+        if (err) {
+            console.error('Error:', err);
+        }
+    });
+}
+
 export function loadDefinitionsByFilename(filename) {
     const definitionId = filename.substr(0, filename.length - 9);
 
     const definitionLoader = new DefinitionLoader();
     definitionLoader.loadDefinition(definitionId);
 
-    return definitionLoader.toObject();
+    return definitionLoader;
 }
 
 export function loadDefinitionsByType(type) {
     const predefined = [];
     let regex = null;
+    let defaultDefinitionLoader = null;
     switch (type) {
         case 'quality':
             regex = /^quality.([A-Za-z0-9_]+).def.json$/;
+            defaultDefinitionLoader = loadDefinitionsByFilename('quality.fast_print.def.json');
             predefined.push('quality.fast_print.def.json');
             predefined.push('quality.normal_quality.def.json');
             predefined.push('quality.high_quality.def.json');
             break;
         case 'material':
             regex = /^material.([A-Za-z0-9_]+).def.json$/;
+            defaultDefinitionLoader = loadDefinitionsByFilename('material.pla.def.json');
             predefined.push('material.pla.def.json');
             predefined.push('material.abs.def.json');
             break;
@@ -171,15 +182,24 @@ export function loadDefinitionsByType(type) {
     const definitions = [];
     for (const filename of predefined) {
         if (includes(filenames, filename)) {
-            const definition = loadDefinitionsByFilename(filename);
-            definitions.push(definition);
+            const definitionLoader = loadDefinitionsByFilename(filename);
+            definitions.push(definitionLoader.toObject());
         }
     }
 
     for (const filename of filenames) {
         if (!includes(predefined, filename) && regex.test(filename)) {
-            const definition = loadDefinitionsByFilename(filename);
-            definitions.push(definition);
+            const definitionLoader = loadDefinitionsByFilename(filename);
+            if (defaultDefinitionLoader) {
+                const ownKeys = Array.from(defaultDefinitionLoader.ownKeys).filter(e => !definitionLoader.ownKeys.has(e));
+                if (ownKeys && ownKeys.length > 0) {
+                    for (const ownKey of ownKeys) {
+                        definitionLoader.ownKeys.add(ownKey);
+                    }
+                    writeDefinition(filename, definitionLoader);
+                }
+            }
+            definitions.push(definitionLoader.toObject());
         }
     }
 
