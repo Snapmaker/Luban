3122fd0959809cd55480fff3719db431a55a5501
parachvte <ryannx6@gmail.com>
2020-03-06 19:26:26 +0800

    Docs: Add commitlint in replace of git-cz as githook (#242)
    * Docs: Add commitlint in replace with git-cz as githook
    
    git-cz intercepts git rebase from use old commit message, and force user
    to write new commit message. This is an unexpected behavior.
    
    Use commitlint we can still use `npm run commit` to help write
    conventional messages, and check the message with commitlint.
    
    * Docs: Update CONTRIBUTING.md on commit messages

---
+++ .commitlintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    rules: {
+        'body-leading-blank': [1, 'always'],
+
+        'footer-leading-blank': [1, 'always'],
+
+        'header-max-length': [2, 'always', 100],
+        'header-min-length': [2, 'always', 20],
+
+        'subject-case': [
+            2,
+            'always',
+            ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+        ],
+        'subject-empty': [2, 'never'],
+        'subject-full-stop': [2, 'never', '.'],
+        'subject-max-length': [2, 'always', 80],
+
+        'type-case': [2, 'always', 'start-case'],
+        'type-empty': [2, 'never'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'Feature',
+                'Improvement',
+                'Fix',
+                'Refactor',
+                'Perf',
+                'Test',
+                'Build',
+                'Style',
+                'Docs'
+            ]
+        ]
+    }
+};
