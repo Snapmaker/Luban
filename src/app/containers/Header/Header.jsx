d3ca4b154843b5d94baf77842a09d7f36d83448a
parachvte <ryannx6@gmail.com>
2020-04-14 21:20:07 +0800

    Refactor: Migrate to bootstrap v4

--- src/app/containers/Header/Header.jsx
+++ src/app/containers/Header/Header.jsx
@@ -1,9 +1,8 @@
-import classNames from 'classnames';
+/* eslint-disable */
 import React, { PureComponent, memo } from 'react';
-import { Nav, Navbar, NavDropdown, MenuItem, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 import semver from 'semver';
-import Push from 'push.js';
 import api from '../../api';
 import Anchor from '../../components/Anchor';
 import settings from '../../config/settings';
@@ -48,19 +47,6 @@ class Header extends PureComponent {
     state = this.getInitialState();
 
     actions = {
-        requestPushPermission: () => {
-            const onGranted = () => {
-                this.setState({ pushPermission: Push.Permission.GRANTED });
-            };
-            const onDenied = () => {
-                this.setState({ pushPermission: Push.Permission.DENIED });
-            };
-            // Note that if "Permission.DEFAULT" is returned, no callback is executed
-            const permission = Push.Permission.request(onGranted, onDenied);
-            if (permission === Push.Permission.DEFAULT) {
-                this.setState({ pushPermission: Push.Permission.DEFAULT });
-            }
-        },
         checkForUpdates: async () => {
             try {
                 const res = await api.getLatestVersion();
@@ -68,32 +54,6 @@ class Header extends PureComponent {
 
                 this._isMounted && this.setState({
                     latestVersion: versions.snapjs
-                });
-            } catch (res) {
-                // Ignore error
-            }
-        },
-        fetchCommands: async () => {
-            try {
-                const res = await api.commands.fetch({ paging: false });
-                const { records: commands } = res.body;
-
-                this._isMounted && this.setState({
-                    commands: commands.filter(command => command.enabled)
-                });
-            } catch (res) {
-                // Ignore error
-            }
-        },
-        runCommand: async (cmd) => {
-            try {
-                const res = await api.commands.run(cmd.id);
-                const { taskID } = res.body;
-
-                this.setState({
-                    commands: this.state.commands.map(c => {
-                        return (c.id === cmd.id) ? { ...c, taskID: taskID, err: null } : c;
-                    })
                 });
             } catch (res) {
                 // Ignore error
@@ -108,101 +68,11 @@ class Header extends PureComponent {
         }
     };
 
-    controllerEvents = {
-        /*
-        'config:change': () => {
-            this.actions.fetchCommands();
-        },
-        'task:start': (taskID) => {
-            this.setState({
-                runningTasks: this.state.runningTasks.concat(taskID)
-            });
-        },
-        'task:finish': (taskID, code) => {
-            const err = (code !== 0) ? new Error(`errno=${code}`) : null;
-            let cmd = null;
-
-            this.setState({
-                commands: this.state.commands.map(c => {
-                    if (c.taskID !== taskID) {
-                        return c;
-                    }
-                    cmd = c;
-                    return {
-                        ...c,
-                        taskID: null,
-                        err: err
-                    };
-                }),
-                runningTasks: without(this.state.runningTasks, taskID)
-            });
-
-            if (cmd && this.state.pushPermission === Push.Permission.GRANTED) {
-                Push.create(cmd.title, {
-                    body: code === 0
-                        ? i18n._('Command succeeded')
-                        : i18n._('Command failed ({{err}})', { err: err }),
-                    icon: 'images/snap-logo-badge-32x32.png',
-                    timeout: 10 * 1000,
-                    onClick: function () {
-                        window.focus();
-                        this.close();
-                    }
-                });
-            }
-        },
-        'task:error': (taskID, err) => {
-            let cmd = null;
-
-            this.setState({
-                commands: this.state.commands.map(c => {
-                    if (c.taskID !== taskID) {
-                        return c;
-                    }
-                    cmd = c;
-                    return {
-                        ...c,
-                        taskID: null,
-                        err: err
-                    };
-                }),
-                runningTasks: without(this.state.runningTasks, taskID)
-            });
-
-            if (cmd && this.state.pushPermission === Push.Permission.GRANTED) {
-                Push.create(cmd.title, {
-                    body: i18n._('Command failed ({{err}})', { err: err }),
-                    icon: 'images/snap-logo-badge-32x32.png',
-                    timeout: 10 * 1000,
-                    onClick: function () {
-                        window.focus();
-                        this.close();
-                    }
-                });
-            }
-        },
-        */
-        // 'workflow:state': (workflowState) => {
-        //     this.setState({ workflowState: workflowState });
-        // }
-    };
-
     _isMounted = false;
 
     getInitialState() {
-        let pushPermission = '';
-        try {
-            // Push.Permission.get() will throw an error if Push is not supported on this device
-            pushPermission = Push.Permission.get();
-        } catch (e) {
-            // Ignore
-        }
-
         return {
             workflowState: controller.workflowState,
-            pushPermission: pushPermission,
-            commands: [],
-            runningTasks: [],
             currentVersion: settings.version,
             latestVersion: settings.version
         };
@@ -212,20 +82,15 @@ class Header extends PureComponent {
         this._isMounted = true;
 
         this.addActionHandlers();
-        this.addControllerEvents();
 
         // Initial actions
         this.actions.checkForUpdates();
-        this.actions.fetchCommands();
     }
 
     componentWillUnmount() {
         this._isMounted = false;
 
         this.removeActionHandlers();
-        this.removeControllerEvents();
-
-        this.runningTasks = [];
     }
 
     addActionHandlers() {
@@ -244,139 +109,52 @@ class Header extends PureComponent {
             });
     }
 
-    addControllerEvents() {
-        Object.keys(this.controllerEvents)
-            .forEach(eventName => {
-                const callback = this.controllerEvents[eventName];
-                controller.on(eventName, callback);
-            });
-    }
-
-    removeControllerEvents() {
-        Object.keys(this.controllerEvents)
-            .forEach(eventName => {
-                const callback = this.controllerEvents[eventName];
-                controller.off(eventName, callback);
-            });
-    }
-
     render() {
-        const { pushPermission, commands, runningTasks, currentVersion, latestVersion } = this.state;
+        const { currentVersion, latestVersion } = this.state;
         const newUpdateAvailable = semver.lt(currentVersion, latestVersion);
 
-        const showCommands = commands.length > 0;
-
         return (
-            <Navbar
-                fixedTop
-                fluid
-                className={styles.navbar}
-            >
-                <Navbar.Header>
+            <Navbar bg="light" expand="lg" fixed="" className={styles['lu-navbar']}>
+                <Navbar.Brand>
                     {newUpdateAvailable && (
                         <OverlayTrigger placement="right" defaultShow overlay={<UpdateTooltip />}>
                             <Logo />
                         </OverlayTrigger>
                     )}
                     {!newUpdateAvailable && <Logo />}
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Nav pullLeft>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="lu-header" />
+                <Navbar.Collapse id="lu-header" className={styles['lu-nav']}>
+                    <Nav className="mr-auto">
                         <MachineSelection />
                     </Nav>
-                    <Nav pullRight>
-                        <NavDropdown
-                            id="nav-dropdown-menu"
-                            title={(
-                                <div title={i18n._('Options')}>
-                                    <i className="fa fa-fw fa-ellipsis-v" />
-                                    {this.state.runningTasks.length > 0 && (
-                                        <span
-                                            className="label label-primary"
-                                            style={{
-                                                position: 'absolute',
-                                                top: 4,
-                                                right: 4
-                                            }}
-                                        >
-                                        N
-                                        </span>
-                                    )}
-                                </div>
-                            )}
-                            noCaret
-                        >
-                            {showCommands && (
-                                <MenuItem header>
-                                    {i18n._('Command')}
-                                    {pushPermission === Push.Permission.GRANTED && (
-                                        <span className="pull-right">
-                                            <i className="fa fa-fw fa-bell-o" />
-                                        </span>
-                                    )}
-                                    {pushPermission === Push.Permission.DENIED && (
-                                        <span className="pull-right">
-                                            <i className="fa fa-fw fa-bell-slash-o" />
-                                        </span>
-                                    )}
-                                    {pushPermission === Push.Permission.DEFAULT && (
-                                        <span className="pull-right">
-                                            <Anchor
-                                                className={styles.btnIcon}
-                                                onClick={this.actions.requestPushPermission}
-                                                title={i18n._('Show notifications')}
-                                            >
-                                                <i className="fa fa-fw fa-bell" />
-                                            </Anchor>
-                                        </span>
-                                    )}
-                                </MenuItem>
-                            )}
-                            {showCommands && commands.map((cmd) => {
-                                const isTaskRunning = runningTasks.indexOf(cmd.taskID) >= 0;
-
-                                return (
-                                    <MenuItem
-                                        key={cmd.id}
-                                        disabled={cmd.disabled}
-                                        onSelect={() => {
-                                            this.actions.runCommand(cmd);
-                                        }}
-                                    >
-                                        <span title={cmd.command}>{cmd.title || cmd.command}</span>
-                                        <span className="pull-right">
-                                            <i
-                                                className={classNames(
-                                                    'fa',
-                                                    'fa-fw',
-                                                    { 'fa-circle-o-notch': isTaskRunning },
-                                                    { 'fa-spin': isTaskRunning },
-                                                    { 'fa-exclamation-circle': cmd.err },
-                                                    { 'text-error': cmd.err }
-                                                )}
-                                                title={cmd.err}
-                                            />
-                                        </span>
-                                    </MenuItem>
-                                );
-                            })}
-                            {showCommands && <MenuItem divider />}
-                            <MenuItem
+                    <Nav className="justify-content-end">
+                        <QuickAccessToolbar state={this.state} actions={this.actions} />
+                        <NavDropdown title={i18n._('More')} alignRight>
+                            <NavDropdown.Item
                                 href="https://store.snapmaker.com"
                                 target="_blank"
                             >
                                 {i18n._('Store')}
-                            </MenuItem>
-                            <MenuItem
+                            </NavDropdown.Item>
+                            <NavDropdown.Item
                                 href="https://snapmaker.com/download"
                                 target="_blank"
                             >
                                 {i18n._('Downloads')}
-                            </MenuItem>
+                            </NavDropdown.Item>
                         </NavDropdown>
+                        <Nav.Item>
+                            <Nav.Link href="https://www.myminifactory.com" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    width="20"
+                                    height="20"
+                                    src="/images/myminifactory-logo-64x64.png"
+                                    alt="Go to MyMiniFactory to find printable objects."
+                                />
+                            </Nav.Link>
+                        </Nav.Item>
                     </Nav>
-                    <QuickAccessToolbar state={this.state} actions={this.actions} />
                 </Navbar.Collapse>
             </Navbar>
         );
