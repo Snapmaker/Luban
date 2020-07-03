c2b9ed4fbd269ed09a83c13a289bb420793867b9
bbaa77770 <444693295@qq.com>
2020-07-03 15:48:43 +0800

    Feature: Change new view of transformation and some functions
    Move x|y into one line. Rotate flip into one line, and its functions. Size add l
    ock button, functions is not finished.

--- src/app/widgets/CncLaserShared/Transformation.jsx
+++ src/app/widgets/CncLaserShared/Transformation.jsx
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Slider from 'rc-slider';
-import Select from 'react-select';
+// import Select from 'react-select';
 import classNames from 'classnames';
 import i18n from '../../lib/i18n';
 import { toFixed } from '../../lib/numeric-utils';
@@ -10,7 +10,6 @@ import Anchor from '../../components/Anchor';
 import TipTrigger from '../../components/TipTrigger';
 import { NumberInput as Input } from '../../components/Input';
 import styles from './styles.styl';
-
 
 class Transformation extends PureComponent {
     static propTypes = {
@@ -75,7 +74,6 @@ class Transformation extends PureComponent {
         const canResize = sourceType !== 'text';
         const selectedNotHide = selectedModelID && !selectedModelHideFlag;
         const actions = this.actions;
-
         return (
             <React.Fragment>
                 <Anchor className="sm-parameter-header" onClick={this.actions.onToggleExpand}>
@@ -91,6 +89,50 @@ class Transformation extends PureComponent {
                 </Anchor>
                 {this.state.expanded && (
                     <React.Fragment>
+                        <TipTrigger
+                            title={i18n._('Move (mm)')}
+                            content={i18n._('Set the coordinate of the selected image or text. You can also drag the image directly.')}
+                        >
+                            <span className="sm-parameter-row__label">{i18n._('Move (mm)')}</span>
+                            <Input
+                                style={{ width: '90px' }}
+                                disabled={!selectedNotHide}
+                                value={toFixed(positionX, 1)}
+                                min={-size.x}
+                                max={size.x}
+                                onChange={(value) => {
+                                    actions.onChangePositionX(value);
+                                    actions.onModelAfterTransform();
+                                }}
+                            />
+                            <span
+                                className={styles['description-text']}
+                                style={{ marginLeft: '-15px', width: '11px', textAlign: 'center' }}
+                            >
+                                X
+                            </span>
+                            <span
+                                className={styles['description-text']}
+                                style={{ width: '27px', textAlign: 'center', display: 'inline-block' }}
+                            />
+                            <Input
+                                style={{ width: '90px' }}
+                                disabled={!selectedNotHide}
+                                value={toFixed(positionY, 1)}
+                                min={-size.y}
+                                max={size.y}
+                                onChange={(value) => {
+                                    actions.onChangePositionY(value);
+                                    actions.onModelAfterTransform();
+                                }}
+                            />
+                            <span
+                                className={styles['description-text']}
+                                style={{ marginLeft: '-15px', width: '11px', textAlign: 'center' }}
+                            >
+                                Y
+                            </span>
+                        </TipTrigger>
                         <TipTrigger
                             title={i18n._('Size')}
                             content={i18n._('Enter the size of the engraved picture. The size cannot be larger than 125 x 125 mm or the size of your material.')}
@@ -110,10 +152,14 @@ class Transformation extends PureComponent {
                                 />
                                 <span
                                     className={styles['description-text']}
-                                    style={{ width: '22px', textAlign: 'center', display: 'inline-block' }}
+                                    style={{ marginLeft: '-15px', width: '11px', textAlign: 'center' }}
                                 >
-                                    X
+                                   W
                                 </span>
+                                <span
+                                    className={styles.icon_size_lock}
+                                    style={{ height: '30px', width: '20px', display: 'inline-block', 'verticalAlign': 'bottom', marginLeft: '2px' }}
+                                />
                                 <Input
                                     style={{ width: '90px' }}
                                     disabled={!selectedNotHide || canResize === false}
@@ -125,6 +171,61 @@ class Transformation extends PureComponent {
                                         actions.onModelAfterTransform();
                                     }}
                                 />
+                                <span
+                                    className={styles['description-text']}
+                                    style={{ marginLeft: '-15px', width: '11px', textAlign: 'center' }}
+                                >
+                                   H
+                                </span>
+                            </div>
+                        </TipTrigger>
+                        <TipTrigger
+                            title={i18n._('Rotate')}
+                            content={i18n._('Rotate the image to the angle you need.')}
+                        >
+                            <div className="sm-parameter-row">
+                                <span className="sm-parameter-row__label">{i18n._('Rotate')}</span>
+                                <Input
+                                    style={{ width: '90px' }}
+                                    disabled={!selectedNotHide}
+                                    value={toFixed(rotationZ * 180 / Math.PI, 1)}
+                                    min={-180}
+                                    max={180}
+                                    onChange={(value) => {
+                                        actions.onChangeRotationZ(value);
+                                        actions.onModelAfterTransform();
+                                    }}
+                                />
+                                <span
+                                    className={styles['description-text']}
+                                    style={{ width: '25px', textAlign: 'center', display: 'inline-block' }}
+                                />
+                                {sourceType === 'raster' && (
+                                    <button
+                                        type="button"
+                                        className={styles.icon_flip_vertically}
+                                        style={(flip & 1) ? { backgroundColor: '#dfdfdf' } : null}
+                                        onClick={() => {
+                                            actions.onChangeFlip({ value: (flip ^ 1) });
+                                            actions.onModelAfterTransform();
+                                        }}
+                                    />
+                                )}
+                                <span
+                                    className={styles['description-text']}
+                                    style={{ width: '25px', textAlign: 'center', display: 'inline-block' }}
+                                />
+                                {sourceType === 'raster' && (
+                                    <button
+                                        type="button"
+                                        className={styles.icon_flip_horizontal}
+                                        style={(flip & 2) ? { backgroundColor: '#dfdfdf' } : null}
+                                        onClick={() => {
+                                            actions.onChangeFlip({ value: (flip ^ 2) });
+                                            actions.onModelAfterTransform();
+                                        }}
+                                    />
+                                )}
                             </div>
                         </TipTrigger>
                         <TipTrigger
@@ -183,68 +284,41 @@ class Transformation extends PureComponent {
                                 />
                             </div>
                         </TipTrigger>
-                        <TipTrigger
-                            title={i18n._('Move Y (mm)')}
-                            content={i18n._('Set the coordinate of the selected image or text in the Y direction. You can also drag the image directly.')}
-                        >
-                            <div className="sm-parameter-row">
-                                <span className="sm-parameter-row__label">{i18n._('Move Y (mm)')}</span>
-                                <Input
-                                    className="sm-parameter-row__slider-input"
-                                    disabled={!selectedNotHide}
-                                    value={toFixed(positionY, 1)}
-                                    min={-size.y}
-                                    max={size.y}
-                                    onChange={(value) => {
-                                        actions.onChangePositionY(value);
-                                        actions.onModelAfterTransform();
-                                    }}
-                                />
-                                <Slider
-                                    className="sm-parameter-row__slider"
-                                    disabled={!selectedNotHide}
-                                    value={positionY}
-                                    min={-size.y}
-                                    max={size.y}
-                                    onChange={actions.onChangePositionY}
-                                    onAfterChange={actions.onModelAfterTransform}
-                                />
-                            </div>
-                        </TipTrigger>
-                        {sourceType === 'raster' && (
-                            <TipTrigger
-                                title={i18n._('Flip Model')}
-                                content={i18n._('Flip the selected Model vertically, horizontally or in both directions.')}
-                            >
-                                <div className="sm-parameter-row">
-                                    <span className="sm-parameter-row__label">{i18n._('Flip Model')}</span>
-                                    <Select
-                                        className="sm-parameter-row__select"
-                                        disabled={!selectedNotHide}
-                                        clearable={false}
-                                        options={[{
-                                            value: 0,
-                                            label: i18n._('None')
-                                        }, {
-                                            value: 1,
-                                            label: i18n._('Vertical')
-                                        }, {
-                                            value: 2,
-                                            label: i18n._('Horizontal')
-                                        }, {
-                                            value: 3,
-                                            label: i18n._('Both')
-                                        }]}
-                                        value={flip}
-                                        seachable={false}
-                                        onChange={(option) => {
-                                            actions.onChangeFlip(option);
-                                            actions.onModelAfterTransform();
-                                        }}
-                                    />
-                                </div>
-                            </TipTrigger>
-                        )}
+                        {/* old flip model*/}
+                        {/*{sourceType === 'raster' && (*/}
+                        {/*    <TipTrigger*/}
+                        {/*        title={i18n._('Flip Model')}*/}
+                        {/*        content={i18n._('Flip the selected Model vertically, horizontally or in both directions.')}*/}
+                        {/*    >*/}
+                        {/*        <div className="sm-parameter-row">*/}
+                        {/*            <span className="sm-parameter-row__label">{i18n._('Flip Model')}</span>*/}
+                        {/*            <Select*/}
+                        {/*                className="sm-parameter-row__select"*/}
+                        {/*                disabled={!selectedNotHide}*/}
+                        {/*                clearable={false}*/}
+                        {/*                options={[{*/}
+                        {/*                    value: 0,*/}
+                        {/*                    label: i18n._('None')*/}
+                        {/*                }, {*/}
+                        {/*                    value: 1,*/}
+                        {/*                    label: i18n._('Vertical')*/}
+                        {/*                }, {*/}
+                        {/*                    value: 2,*/}
+                        {/*                    label: i18n._('Horizontal')*/}
+                        {/*                }, {*/}
+                        {/*                    value: 3,*/}
+                        {/*                    label: i18n._('Both')*/}
+                        {/*                }]}*/}
+                        {/*                value={flip}*/}
+                        {/*                seachable={false}*/}
+                        {/*                onChange={(option) => {*/}
+                        {/*                    actions.onChangeFlip(option);*/}
+                        {/*                    actions.onModelAfterTransform();*/}
+                        {/*                }}*/}
+                        {/*            />*/}
+                        {/*        </div>*/}
+                        {/*    </TipTrigger>*/}
+                        {/*)}*/}
                     </React.Fragment>
                 )}
             </React.Fragment>
