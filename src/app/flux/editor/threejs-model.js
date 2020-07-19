656f4b5e73251fb7ff923fe38e92a8fae6b16aaf
jane <jiantaolovebingbing@gmail.com>
2020-07-19 23:28:51 +0800

    Fix: Fix adapt fontsize to text's height

--- src/app/flux/editor/threejs-model.js
+++ src/app/flux/editor/threejs-model.js
@@ -39,18 +39,30 @@ export const threejsModelActions = {
     // callback
 
     updateSelectedModelTransformation: (headType, transformation) => (dispatch, getState) => {
-        const { modelGroup, toolPathModelGroup, config } = getState()[headType];
+        const { modelGroup, toolPathModelGroup, svgModelGroup, config } = getState()[headType];
         const modelState = modelGroup.updateSelectedModelTransformation(transformation);
+        const selectedModel = modelGroup.getSelectedModel();
 
         const { text, lineHeight, size } = config;
         if (text) {
             const numberOfLines = text.split('\n').length;
             const estimatedHeight = numberOfLines === 1 ? transformation.height : transformation.height / (1 + lineHeight * (numberOfLines - 1));
             const newSize = (estimatedHeight * 72 / 25.4);
+
             if (Math.abs(newSize - size) > EPSILON) {
+                const source = {
+                    width: selectedModel.transformation.width,
+                    height: selectedModel.transformation.height
+                };
+                svgModelGroup.updateTransformation(transformation);
+                modelGroup.updateSelectedSource(source);
+                modelGroup.updateSelectedModelTransformation(transformation);
+                modelGroup.updateSelectedConfig({ size: Math.ceil(newSize) });
+                dispatch(baseActions.updateConfig(headType, { size: Math.ceil(newSize) }));
                 dispatch(baseActions.updateConfig(headType, { size: Math.ceil(newSize) }));
             }
         }
+
         if (modelState) {
             toolPathModelGroup.updateSelectedNeedPreview(true);
             dispatch(baseActions.updateTransformation(headType, modelState.transformation));
