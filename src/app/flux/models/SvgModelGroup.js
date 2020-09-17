5648a881d85794496909857a66026cc7269ad758
bbaa77770 <444693295@qq.com>
2020-09-17 12:22:23 +0800

    Fix: Remove unused function

--- src/app/flux/models/SvgModelGroup.js
+++ src/app/flux/models/SvgModelGroup.js
@@ -406,6 +406,7 @@ class SvgModelGroup {
                 height: posAndSize.height
             });
         }
+        // this.invokeModelTransformCallback();
     }
 
     // when single select
@@ -740,9 +741,9 @@ class SvgModelGroup {
         this.modelTransformCallback = callback;
     }
 
-    // invokeModelTransformCallback() {
-    //     this.modelTransformCallback && this.modelTransformCallback();
-    // }
+    invokeModelTransformCallback() {
+        this.modelTransformCallback && this.modelTransformCallback();
+    }
 }
 
 export default SvgModelGroup;
