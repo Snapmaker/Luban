50abfca766534853e7baf265d7af03b270b3f164
Kyle Cooke <cookekyle97@gmail.com>
2020-10-12 04:01:45 +0100

    Improvement: Convert SVG's component paths one at a time (#472)

--- src/server/lib/ToolPathGenerator/SVGFill.js
+++ src/server/lib/ToolPathGenerator/SVGFill.js
@@ -174,13 +174,18 @@ function svgToSegments(svg, options = {}) {
         const color = 1; // only black & white, use 1 to mark canvas as painted
         const width = Math.round(options.width * options.fillDensity);
         const height = Math.round(options.height * options.fillDensity);
-
-        const canvas = new Array(width);
+        let out = [];
+        const accumulatorCanvas = new Array(width);
         for (let i = 0; i < width; i++) {
-            canvas[i] = new Uint8Array(height);
+            accumulatorCanvas[i] = new Uint8Array(height);
         }
 
         for (const shape of svg.shapes) {
+            const canvas = new Array(width);
+            for (let i = 0; i < width; i++) {
+                canvas[i] = new Uint8Array(height);
+            }
+
             if (!shape.visibility) {
                 continue;
             }
@@ -210,9 +215,20 @@ function svgToSegments(svg, options = {}) {
                 }
                 fillShape(canvas, width, height, newShape, color);
             }
+            for (const [rowIndex, row] of canvas.entries()) {
+                for (const [columnIndex, value] of row.entries()) {
+                    if (accumulatorCanvas[rowIndex][columnIndex]) {
+                        canvas[rowIndex][columnIndex] = 0;
+                    }
+                    if (value) {
+                        accumulatorCanvas[rowIndex][columnIndex] = value;
+                    }
+                }
+            }
+            out = [...out, ...canvasToSegments(canvas, width, height, options.fillDensity)];
         }
 
-        return canvasToSegments(canvas, width, height, options.fillDensity);
+        return out;
     }
 }
 
