41b635a97827e9d787cfd5f2cecd1567bcf5d56b
jane <33253363+jane-rose@users.noreply.github.com>
2021-01-08 18:13:50 +0800

    Fix: Fix connecting Wi-Fi manually blanks out software (#685)

--- src/app/widgets/Connection/WifiConnection.jsx
+++ src/app/widgets/Connection/WifiConnection.jsx
@@ -247,7 +247,7 @@ class WifiConnection extends PureComponent {
         } else {
             find = servers.find(v => v.name === server.name && v.address === server.address);
         }
-        if (find) {
+        if (find && find !== server) {
             this.setState({
                 server: find
             });
