e6e2d4ad518175875aad7af6377ecd1fc5fd7116
bbaa77770 <444693295@qq.com>
2021-01-29 12:00:54 +0800

    Fix: Fix Emergency stop error in wifi

--- src/app/flux/machine/index.js
+++ src/app/flux/machine/index.js
@@ -683,14 +683,14 @@ export const actions = {
     },
 
     close: (options, isEmergencyStopped) => (dispatch, getState) => {
-        const { port } = options;
         const state = getState().machine;
         const ports = [...state.ports];
-        const portIndex = ports.indexOf(port);
-        if (portIndex !== -1) {
-            ports.splice(portIndex, 1);
-        }
         if (!isEmpty(ports)) {
+            const { port } = options;
+            const portIndex = ports.indexOf(port);
+            if (portIndex !== -1) {
+                ports.splice(portIndex, 1);
+            }
             // this.port = ports[0];
             dispatch(baseActions.updateState({
                 port: ports[0],
