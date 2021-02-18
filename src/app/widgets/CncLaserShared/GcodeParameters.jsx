e9f8f5bd9b866e09244ffaa29dd8f1d25c7afeea
Longbot <444693295@qq.com>
2021-02-18 10:44:32 +0800

    Fix: Fix some styles after import the language German and French (#761)
    Co-authored-by: parachvte <ryannx6@gmail.com>

--- src/app/widgets/CncLaserShared/GcodeParameters.jsx
+++ src/app/widgets/CncLaserShared/GcodeParameters.jsx
@@ -261,7 +261,7 @@ class GcodeParameters extends PureComponent {
                             <OptionalDropdown
                                 style={{ marginTop: '10px' }}
                                 title={i18n._('Fixed Power')}
-                                titleTip={i18n._('When enabled, the power used to engrave this image will be set in the G-code, so it is not affected by the power you set in Workspace. When engraving multiple images, you can set the power for each image separately.')}
+                                titleTip={i18n._('When enabled, the power used to engrave this image will be set in the G-code, so it is not affected by the power you set in Workspace. When engraving multiple images, you can set the power for each image individually.')}
                                 disabled={disabled}
                                 onClick={actions.onToggleFixedPowerEnabled}
                                 hidden={!fixedPowerEnabled}
