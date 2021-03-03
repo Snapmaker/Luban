837d45194ada0748c20b5fd0e23cdf4d66ad59ea
meiliang1123 <meiliang1123@126.com>
2021-03-03 11:09:11 +0800

    Fix: Destroy Gcodeline when starting support mode

--- src/app/widgets/PrintingVisualizer/Visualizer.jsx
+++ src/app/widgets/PrintingVisualizer/Visualizer.jsx
@@ -33,6 +33,7 @@ class Visualizer extends PureComponent {
         displayedType: PropTypes.string.isRequired,
         renderingTimestamp: PropTypes.number.isRequired,
 
+        destroyGcodeLine: PropTypes.func.isRequired,
         selectMultiModel: PropTypes.func.isRequired,
         removeSelectedModel: PropTypes.func.isRequired,
         removeAllModels: PropTypes.func.isRequired,
@@ -152,6 +153,7 @@ class Visualizer extends PureComponent {
             return this.state.isSupporting;
         },
         startSupportMode: () => {
+            this.props.destroyGcodeLine();
             this.actions.setTransformMode('support');
             this.setState({ isSupporting: true });
             this.canvas.current.controls.startSupportMode();
@@ -462,6 +464,7 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
+    destroyGcodeLine: () => dispatch(printingActions.destroyGcodeLine()),
     selectMultiModel: (intersect, selectEvent) => dispatch(printingActions.selectMultiModel(intersect, selectEvent)),
     removeSelectedModel: () => dispatch(printingActions.removeSelectedModel()),
     removeAllModels: () => dispatch(printingActions.removeAllModels()),
