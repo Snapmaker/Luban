436dbd5914662dfed54d31bffba5ced828cfd66f
zjiefee <zjief1992@gmail.com>
2021-03-10 18:41:50 +0800

    Fix: Fix bug for CNC relief tool path (#836)

--- src/server/lib/ToolPathGenerator/CncReliefToolPathGenerator.js
+++ src/server/lib/ToolPathGenerator/CncReliefToolPathGenerator.js
@@ -560,6 +560,7 @@ export default class CncReliefToolPathGenerator extends EventEmitter {
                             z: k === 0 ? this.initialZ : z,
                             f: jogSpeed
                         });
+                        currentZ = k === 0 ? this.initialZ : z;
                     }
                 }
 
