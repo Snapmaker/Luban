7d4abb77f67915660141b9fbbaae07b077cb6165
meiliang1123 <meiliang1123@126.com>
2021-03-12 11:26:16 +0800

    Fix: Fix remaining time displayed error (#858)

--- src/app/flux/machine/Server.js
+++ src/app/flux/machine/Server.js
@@ -685,7 +685,7 @@ export class Server extends events.EventEmitter {
         let remainingTime = 0;
         if (this.state.gcodePrintingInfo.startTime) {
             elapsedTime = new Date().getTime() - this.state.gcodePrintingInfo.startTime;
-            remainingTime = estimatedTime - elapsedTime;
+            remainingTime = estimatedTime * 1000 - elapsedTime;
         }
         let finishTime = 0;
         if (received > 0 && received >= totalLines) {
