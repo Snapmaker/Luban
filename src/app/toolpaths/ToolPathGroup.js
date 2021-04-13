c68f80f59cbfadb6fda67d4e6c0db1480e0940ec
Longbot <444693295@qq.com>
2021-04-13 11:54:29 +0800

    Update ToolPathGroup.js

--- src/app/toolpaths/ToolPathGroup.js
+++ src/app/toolpaths/ToolPathGroup.js
@@ -42,14 +42,14 @@ class ToolPathGroup {
     }
 
     /**
-    Used to show tool path group objects in process canvas after preview models
+     * Used to show tool path group objects in process canvas after preview models
      */
     show() {
         this.object.visible = true;
     }
 
     /**
-     *  Used to show model group objects in process canvas before preview models
+     * Used to show model group objects in process canvas before preview models
      */
     hide() {
         this.object.visible = false;
