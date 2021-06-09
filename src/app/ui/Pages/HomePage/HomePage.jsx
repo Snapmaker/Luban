c59bccf01af03c102c9a7097c32ef8275df282b4
wenqing <wenqing@snapmaker.com>
2021-06-09 10:01:18 +0800

    Feature: Add setting guide

--- src/app/ui/Pages/HomePage/HomePage.jsx
+++ src/app/ui/Pages/HomePage/HomePage.jsx
@@ -1,14 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import isElectron from 'is-electron';
+import i18next from 'i18next';
+import { gte } from 'lodash';
 import styles from './styles.styl';
 import HomeLayout from '../../Layouts/HomeLayout';
+import { machineStore } from '../../../store/local-storage';
 
 // component
 import Begin from './Begin';
 import QuickStart from './QuickStart';
 import MoreInfo from './MoreInfo';
+import SettingGuideModal from './SettingGuideModal';
 
 const HomePage = (props) => {
+    const [modalShow, setModalShow] = useState(false);
+    useEffect(() => {
+        const settingStore = machineStore.get('settings');
+        if (gte(machineStore.version, '3.16.0') && (!settingStore?.finishGuide || settingStore?.guideVersion !== 1)) {
+            setModalShow(true);
+        } else {
+            setModalShow(false);
+        }
+    }, []);
     useEffect(() => {
         if (isElectron()) {
             const ipc = window.require('electron').ipcRenderer;
@@ -32,11 +45,20 @@ const HomePage = (props) => {
             </div>
         );
     };
+    const renderModalView = () => {
+        return (
+            <SettingGuideModal
+                handleModalShow={setModalShow}
+                initLanguage={i18next.language}
+            />
+        );
+    };
     return (
         <HomeLayout
             className={styles.homepageContainer}
             renderMiddleView={renderMiddleView}
             renderBottomBar={renderBottomBar}
+            renderModalView={modalShow ? renderModalView : null}
         />
     );
 };
