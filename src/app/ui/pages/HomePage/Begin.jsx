6524d578c50c22409fb11726c17977406a870de7
owen <xkfront@outlook.com>
2021-08-05 16:56:31 +0800

    Fix: New file from appbar menu incorrect (#1110)
    * Fix: Incorrect new file from appmenu
    
    * Fix: Homepage recent file name duplicate

--- src/app/ui/pages/HomePage/Begin.jsx
+++ src/app/ui/pages/HomePage/Begin.jsx
@@ -10,9 +10,8 @@ import Anchor from '../../components/Anchor';
 import styles from './styles.styl';
 import i18n from '../../../lib/i18n';
 import { actions as projectActions } from '../../../flux/project';
-import { actions as editorActions } from '../../../flux/editor';
 import Workspace from '../Workspace';
-import { COORDINATE_MODE_CENTER, COORDINATE_MODE_BOTTOM_CENTER, HEAD_LASER, HEAD_CNC } from '../../../constants';
+import { HEAD_LASER, HEAD_CNC, HEAD_3DP } from '../../../constants';
 import UniApi from '../../../lib/uni-api';
 
 const Begin = () => {
@@ -21,17 +20,11 @@ const Begin = () => {
     const history = useHistory();
     const location = useLocation();
     const project = useSelector(state => state?.project);
-    const store = useSelector(state => state);
     const newRecentFile = reverse(cloneDeep(project.general.recentFiles));
     // useState
     const [showWorkspace, setShowWorkspace] = useState(false);
     const [beginSelected, setBeginSelected] = useState('start-project');
     // method
-    const onStartProject = async (pathname) => {
-        const oldPathname = location?.pathname;
-        await dispatch(projectActions.startProject(oldPathname, pathname, history));
-        return null;
-    };
     function renderWorkspace() {
         const onClose = () => setShowWorkspace(false);
         return showWorkspace && renderPopup({
@@ -48,30 +41,6 @@ const Begin = () => {
         }
     }
 
-    const changeAxis = async (e, isRotate, headType) => {
-        e.preventDefault();
-        if (!isRotate) {
-            const { materials } = store?.[headType];
-            if (materials.isRotate !== isRotate) {
-                await dispatch(editorActions.changeCoordinateMode(headType, COORDINATE_MODE_CENTER));
-                await dispatch(editorActions.updateMaterials(headType, { isRotate }));
-            }
-        } else {
-            const { SVGActions, materials } = store?.[headType];
-            if (materials.isRotate !== isRotate) {
-                await dispatch(editorActions.changeCoordinateMode(
-                    headType,
-                    COORDINATE_MODE_BOTTOM_CENTER, {
-                        x: materials.diameter * Math.PI,
-                        y: materials.length
-                    },
-                    !SVGActions.svgContentGroup
-                ));
-                await dispatch(editorActions.updateMaterials(headType, { isRotate }));
-            }
-        }
-    };
-
     const onClickToUpload = () => {
         UniApi.Event.emit('appbar-menu:open-file-in-browser');
     };
@@ -80,9 +49,8 @@ const Begin = () => {
         setBeginSelected(type);
     };
 
-    const handleChangeAxis = async (e, isRotate, headType) => {
-        await changeAxis(e, isRotate, headType);
-        onStartProject(`/${headType}`);
+    const handleChangeAxis = async (isRotate, headType) => {
+        UniApi.Event.emit('appbar-menu:new-file', { headType, isRotate });
     };
 
     return (
@@ -105,7 +73,7 @@ const Begin = () => {
                     {beginSelected === 'start-project' && (
                         <div className={classNames(styles['link-bar'], 'margin-vertical-48')}>
                             <div className={classNames(styles['3dp'], 'margin-horizontal-16')}>
-                                <Anchor onClick={() => onStartProject('/3dp')} title={i18n._('3D Printing G-code Generator')}>
+                                <Anchor onClick={() => handleChangeAxis(false, HEAD_3DP)} title={i18n._('3D Printing G-code Generator')}>
                                     <div className={classNames(styles.imgWrapper)}>
                                         <img src={require('./images/icon_3d_120x120.svg')} alt="" />
                                     </div>
@@ -117,10 +85,10 @@ const Begin = () => {
                                     <div className={classNames(styles.imgWrapper)}>
                                         <img className={classNames(styles['laser-img'])} src={require('./images/icon_laser_120x120.svg')} alt="" />
                                         <div className={styles['laser-axis-select']}>
-                                            <Button onClick={(e) => { handleChangeAxis(e, false, HEAD_LASER); }} className={classNames(styles['three-axis-select'])} type="default" priority="level-three">
+                                            <Button onClick={() => { handleChangeAxis(false, HEAD_LASER); }} className={classNames(styles['three-axis-select'])} type="default" priority="level-three">
                                                 {i18n._('3-axis')}
                                             </Button>
-                                            <Button onClick={(e) => { handleChangeAxis(e, true, HEAD_LASER); }} className={classNames(styles['four-axis-select'])} type="default" priority="level-three">
+                                            <Button onClick={() => { handleChangeAxis(true, HEAD_LASER); }} className={classNames(styles['four-axis-select'])} type="default" priority="level-three">
                                                 {i18n._('4-axis')}
                                             </Button>
                                         </div>
@@ -133,10 +101,10 @@ const Begin = () => {
                                     <div className={classNames(styles.imgWrapper)}>
                                         <img className={classNames(styles['cnc-img'])} src={require('./images/icon_cnc_120x120.svg')} alt="" />
                                         <div className={styles['cnc-axis-select']}>
-                                            <Button onClick={(e) => { handleChangeAxis(e, false, HEAD_CNC); }} className={classNames(styles['three-axis-select'])} type="default" priority="level-three">
+                                            <Button onClick={() => { handleChangeAxis(false, HEAD_CNC); }} className={classNames(styles['three-axis-select'])} type="default" priority="level-three">
                                                 {i18n._('3-axis')}
                                             </Button>
-                                            <Button onClick={(e) => { handleChangeAxis(e, true, HEAD_CNC); }} className={classNames(styles['four-axis-select'])} type="default" priority="level-three">
+                                            <Button onClick={() => { handleChangeAxis(true, HEAD_CNC); }} className={classNames(styles['four-axis-select'])} type="default" priority="level-three">
                                                 {i18n._('4-axis')}
                                             </Button>
                                         </div>
