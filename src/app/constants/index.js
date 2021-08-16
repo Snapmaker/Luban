8bbc0beee9f654a99dfb07b5154a536e25994174
wenqing <wenqing@snapmaker.com>
2021-08-16 10:02:57 +0800

    Fix: Update Video Tutorial link

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -842,7 +842,7 @@ export const MIN_LASER_CNC_CANVAS_SCALE = 0.5;
 export const FORUM_URL = 'https://forum.snapmaker.com/';
 export const SUPPORT_ZH_URL = 'https://support.snapmaker.com/hc/zh-cn';
 export const SUPPORT_EN_URL = 'https://support.snapmaker.com/hc/en-us';
-export const TUTORIAL_VIDEO_URL = 'https://www.youtube.com/c/Snapmaker/playlists';
+export const TUTORIAL_VIDEO_URL = 'https://www.youtube.com/playlist?list=PLEn5aHQNSrHWvLWgQwrnLPY6VcaYnTvcQ';
 export const OFFICIAL_SITE_ZH_URL = 'https://snapmaker.cn/';
 export const OFFICIAL_SITE_EN_URL = 'https://snapmaker.com/';
 export const MARKET_ZH_URL = 'https://snapmaker.world.tmall.com/?spm=a1z10.3-b.w5001-21696184167.3.40be7f386PAuCQ&scene=taobao_shop';
