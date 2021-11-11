46dc726fa73a192c365d74082651bdb3c2433f21
wenqing <wenqing@snapmaker.com>
2021-11-11 17:18:09 +0800

    Fix: Fix: modal style

--- src/app/ui/widgets/WorkingProgress/WorkingProgress.jsx
+++ src/app/ui/widgets/WorkingProgress/WorkingProgress.jsx
@@ -11,7 +11,7 @@ const Text = ({ name, value }) => {
     return (
         <div className="margin-top-8 sm-flex align-c">
             <span className="unit-text margin-right-8 text-overflow-ellipsis display-inline align-l">{name}</span>
-            <span className="main-text-normal text-overflow-ellipsis display-inline width-136 align-l">{value}</span>
+            <span className="main-text-normal text-overflow-ellipsis display-inline width-112 align-l">{value}</span>
         </div>
     );
 };
@@ -62,7 +62,7 @@ const WorkingProgress = ({ widgetActions, controlActions }) => {
 
     return (
         <div>
-            <div className="sm-flex justify-space-between align-center margin-top-8 margin-bottom-16">
+            <div className="sm-flex justify-space-between align-center margin-top-8">
                 <div>
                     <Text name={i18n._('key-Workspace/Workprogress-File')} value={fileName || i18n._('key-Workspace/Workprogress-Unknow')} />
                     <Text name={i18n._('key-Workspace/Workprogress-Elapsed Time')} value={formatDuration(elapsedTime, false)} />
@@ -72,7 +72,7 @@ const WorkingProgress = ({ widgetActions, controlActions }) => {
                 <Progress percent={Math.floor(progress ? progress * 100 : (sent / total) * 100)} type="circle" width={88} />
             </div>
             {!(sent >= total && total !== 0) && (
-                <div className="sm-flex justify-space-between align-center">
+                <div className="sm-flex justify-space-between align-center margin-top-16">
                     <Button width="160px" type="default" onClick={() => handleMachine(currentWorkflowStatus === 'running' ? 'pause' : 'run')}>
                         <SvgIcon
                             name={currentWorkflowStatus === 'running' ? 'WorkspaceSuspend' : 'WorkspacePlay'}
