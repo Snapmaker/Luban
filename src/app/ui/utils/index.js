662e49de47cf6673193c2dba62377e0770418b21
mashizhen <dohard@163.com>
2021-11-29 17:43:12 +0800

    Feature: Add laser firmware update modal

--- src/app/ui/utils/index.js
+++ src/app/ui/utils/index.js
@@ -1,4 +1,5 @@
 import renderModal from './renderModal';
+import maxZindex from './max-zindex';
 import renderPopup from './renderPopup';
 import renderWidgetList from './renderWidgetList';
 import { useRecoveringProject, useRenderRecoveryModal } from './pluginHooksAutoRecovery';
@@ -6,11 +7,12 @@ import { useUnsavedTitle } from './pluginUnsaveTitle';
 import renderRecoveryModal from './renderRecoveryModal';
 import { logPageView } from './gaEvent';
 
-export { renderModal, renderPopup, renderWidgetList, renderRecoveryModal,
+export { renderModal, maxZindex, renderPopup, renderWidgetList, renderRecoveryModal,
     useUnsavedTitle, logPageView, useRecoveringProject, useRenderRecoveryModal };
 
 export default {
     renderModal,
+    maxZindex,
     renderPopup,
     renderWidgetList,
     renderRecoveryModal,
