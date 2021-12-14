c6fd676ee25b2780a1db8542ecf7729ae04fe3ed
jane <jiantaolovebingbing@gmail.com>
2021-12-14 16:12:52 +0800

    Refactor: Refactor to change icon

--- src/app/ui/pages/Printing.jsx
+++ src/app/ui/pages/Printing.jsx
@@ -172,7 +172,7 @@ function useRenderMainToolBar() {
                 title: i18n._('key-3DP/MainToolBar-Align'),
                 disabled: !canMerge || !enableShortcut,
                 type: 'button',
-                name: 'MainToolbarUngroup',
+                name: 'MainToolbarMerge',
                 action: () => {
                     dispatch(printingActions.groupAndAlign());
                 }
