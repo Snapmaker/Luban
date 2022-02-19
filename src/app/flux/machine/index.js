0c553f6f064863733885f0173be6df9ad75f876c
jane <jiantaolovebingbing@gmail.com>
2022-02-19 19:31:00 +0800

    Refactor: Refactor to remove forbid gcode except idle condition

--- src/app/flux/machine/index.js
+++ src/app/flux/machine/index.js
@@ -877,7 +877,7 @@ export const actions = {
 
     executeGcode: (gcode, context) => (dispatch, getState) => {
         const machine = getState().machine;
-        const { homingModal, workflowState } = machine;
+        const { homingModal } = machine;
         const { isConnected, connectionType, server } = machine;
         if (!isConnected) {
             if (homingModal) {
@@ -890,9 +890,9 @@ export const actions = {
         // if (port && workflowState === WORKFLOW_STATE_IDLE) {
         if (connectionType === CONNECTION_TYPE_SERIAL) {
             // controller.command('gcode', gcode, context);
-            if (workflowState !== WORKFLOW_STATE_IDLE) {
-                return;
-            }
+            // if (workflowState !== WORKFLOW_STATE_IDLE) {
+            //     return;
+            // }
             controller.command('gcode', gcode, context);
             // } else if (server && workflowStatus === STATUS_IDLE) {
         } else {
