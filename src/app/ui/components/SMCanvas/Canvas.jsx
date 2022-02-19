6560f5924fed6d7c2f71f5a81f43081266ee48ce
jane <jiantaolovebingbing@gmail.com>
2022-02-19 20:06:14 +0800

    Refactor: Refactor to add light for 3D printing support

--- src/app/ui/components/SMCanvas/Canvas.jsx
+++ src/app/ui/components/SMCanvas/Canvas.jsx
@@ -8,7 +8,7 @@
 import noop from 'lodash/noop';
 import React, { PureComponent } from 'react';
 import { isNil, throttle } from 'lodash';
-import { Vector3, PerspectiveCamera, Scene, Group,
+import { Vector3, PerspectiveCamera, Scene, Group, AmbientLight, PointLight,
     HemisphereLight, DirectionalLight } from 'three';
 import PropTypes from 'prop-types';
 import TWEEN from '@tweenjs/tween.js';
@@ -240,12 +240,12 @@ class Canvas extends PureComponent {
             this.light = new DirectionalLight(0xffffff, 0.6);
             this.light.position.copy(this.cameraInitialPosition);
         }
-        // if (this.transformSourceType === '3D') {
-        //     this.light = new DirectionalLight(0x666666, 0.4);
-        //     const pLight = new PointLight(0xffffff, 0.60, 0, 0.60);
-        //     this.camera.add(pLight);
-        //     pLight.position.copy(new Vector3(-4000, 7000, 50000));
-        // }
+        if (this.transformSourceType === '3D') {
+            this.light = new DirectionalLight(0x666666, 0.4);
+            const pLight = new PointLight(0xffffff, 0.60, 0, 0.60);
+            this.camera.add(pLight);
+            pLight.position.copy(new Vector3(-4000, 7000, 50000));
+        }
 
         // We need to change the default up vector if we use camera to respect XY plane
         if (this.props.cameraUp) {
@@ -263,14 +263,14 @@ class Canvas extends PureComponent {
         this.group.position.copy(DEFAULT_MODEL_POSITION);
         this.scene.add(this.group);
 
-        // if (this.transformSourceType === '3D') {
-        //     const lightTop = new HemisphereLight(0xA3A3A3, 0x545454, 0.5);
-        //     const lightInside = new AmbientLight(0x666666);
-        //     lightTop.position.copy(new Vector3(0, 0, -49000));
-        //     lightInside.position.copy(new Vector3(0, 0, 0));
-        //     this.scene.add(lightTop);
-        //     this.scene.add(lightInside);
-        // }
+        if (this.transformSourceType === '3D') {
+            const lightTop = new HemisphereLight(0xA3A3A3, 0x545454, 0.5);
+            const lightInside = new AmbientLight(0x666666);
+            lightTop.position.copy(new Vector3(0, 0, -49000));
+            lightInside.position.copy(new Vector3(0, 0, 0));
+            this.scene.add(lightTop);
+            this.scene.add(lightInside);
+        }
         if (this.transformSourceType === '2D') {
             this.scene.add(new HemisphereLight(0x000000, 0xcecece));
         }
