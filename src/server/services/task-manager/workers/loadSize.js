c0d00924e336aac17d868a1a8b923a1a33bd6334
MaShizhen <dohard@163.com>
2022-02-24 15:27:11 +0800

    Fix: Fix error listening for loadsize worker (#1476)

--- src/server/services/task-manager/workers/loadSize.js
+++ src/server/services/task-manager/workers/loadSize.js
@@ -2,8 +2,12 @@ import { Mesh } from '../../../lib/MeshProcess/Mesh';
 import sendMessage from '../utils/sendMessage';
 
 const loadSize = ({ tempName, isRotate }) => {
-    const { width, height } = Mesh.loadSize(`${process.env.Tmpdir}/${tempName}`, isRotate === 'true' || isRotate === true);
+    try {
+        const { width, height } = Mesh.loadSize(`${process.env.Tmpdir}/${tempName}`, isRotate === 'true' || isRotate === true);
 
-    sendMessage({ status: 'complete', width, height });
+        sendMessage({ status: 'complete', width, height });
+    } catch (error) {
+        sendMessage({ status: 'fail', error });
+    }
 };
 export default loadSize;
