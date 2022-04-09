552869d53d8ed8174d605b02e10d2a6402f3ec21
xiekun1992 <xkfront@outlook.com>
2022-04-09 15:09:27 +0800

    Fix: Fix select dropdown filter unavailable

--- src/app/ui/views/ProfileManager/ConfigValueBox.jsx
+++ src/app/ui/views/ProfileManager/ConfigValueBox.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { includes, throttle } from 'lodash';
+import { includes } from 'lodash';
 import classNames from 'classnames';
 import i18n from '../../../lib/i18n';
 import SettingItem from './SettingItem';
@@ -10,21 +10,22 @@ import styles from './styles.styl';
 import SvgIcon from '../../components/SvgIcon';
 
 function ConfigValueBox({ optionConfigGroup, calculateTextIndex, isCategorySelected, type = 'input', isOfficialDefinition = () => true, onChangeDefinition, selectedSettingDefaultValue, definitionForManager, customConfigs, showMiddle = false, hideMiniTitle = false }) {
-    const [activeCateId, setActiveCateId] = useState(0);
+    const [activeCateId, setActiveCateId] = useState(2);
     const scrollDom = useRef(null);
     const fieldsDom = useRef([]);
     function setActiveCate(cateId) {
         if (scrollDom.current) {
             const container = scrollDom.current.parentElement;
-            const offsetTops = [...scrollDom.current.children].map(i => i.offsetTop - 92);
+            const offsetTops = [...scrollDom.current.children].map(i => i.offsetTop);
             if (cateId !== undefined) {
-                container.scrollTop = offsetTops[cateId];
+                container.scrollTop = offsetTops[cateId] - 80;
             } else {
                 cateId = offsetTops.findIndex((item, idx) => item < container.scrollTop && offsetTops[idx + 1] > container.scrollTop);
                 cateId = Math.max(cateId, 0);
             }
             setActiveCateId(cateId);
         }
+        return true;
     }
     useEffect(() => {
         fieldsDom.current = fieldsDom.current.slice(0, Object.keys(optionConfigGroup).length);
@@ -67,13 +68,9 @@ function ConfigValueBox({ optionConfigGroup, calculateTextIndex, isCategorySelec
             )}
             <div
                 className={classNames(styles['manager-details'], 'border-default-grey-1', 'border-radius-8')}
-                onWheel={
-                    throttle(() => {
-                        setActiveCate();
-                    }, 200, { 'leading': false, 'trailing': true })
-                }
+                onWheel={() => { setActiveCate(); }}
             >
-                <div className="sm-parameter-container position-re" ref={scrollDom}>
+                <div className="sm-parameter-container" ref={scrollDom}>
                     {!isCategorySelected && optionConfigGroup.map((group, index) => {
                         const eachFieldsDom = fieldsDom.current[index];
                         return (
@@ -96,7 +93,6 @@ function ConfigValueBox({ optionConfigGroup, calculateTextIndex, isCategorySelec
                                                     <SettingItem
                                                         settings={definitionForManager?.settings}
                                                         definitionKey={key}
-                                                        ref={scrollDom}
                                                         width="160px"
                                                         key={key}
                                                         isDefaultDefinition={isEditable}
