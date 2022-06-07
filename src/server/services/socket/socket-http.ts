6575237785ee907a0ebc657523faa21903f9cbf8
jane <33253363+jane-rose@users.noreply.github.com>
2022-06-07 17:46:06 +0800

    Fix: Fix firmware compatibility and maintool bar bug on macOS (#1608)
    * Fix: Fix firmware compatibility
    
    * Fix: Add submodule token to fix packaging (#1598)
    
    * Fix: Add submodule token to fix packaging

--- src/server/services/socket/socket-http.ts
+++ src/server/services/socket/socket-http.ts
@@ -78,7 +78,6 @@ const _getResult = (err, res) => {
     };
 };
 // let timeoutHandle = null;
-let intervalHandle = null;
 
 export type StateOptions = {
     headType?: string,
@@ -140,7 +139,6 @@ class SocketHttp {
         this.init();
         log.debug(`wifi host="${this.host}" : token=${this.token}`);
         const api = `${this.host}/api/v1/connect`;
-        intervalHandle = setInterval(this.getEnclosureStatus, 1000);
         request
             .post(api)
             .timeout(3000)
@@ -151,6 +149,8 @@ class SocketHttp {
                 }
                 if (err) {
                     log.debug(`err="${err}"`);
+                } else {
+                    this.getEnclosureStatus();
                 }
                 const result = _getResult(err, res);
                 const { data } = result;
@@ -204,7 +204,6 @@ class SocketHttp {
             this.host = '';
             this.token = '';
             this.heartBeatWorker && this.heartBeatWorker.terminate();
-            clearInterval(intervalHandle);
         } else {
             socket && socket.emit(eventName, _getResult(new Error('connection not exist'), null));
         }
