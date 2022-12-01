2f19c822fb7743e199a8b2038742d882687d6ba6
parachvte <ryannx6@gmail.com>
2022-12-01 16:29:23 +0800

    Refactor: Upgrade babel config

--- babel.config.js
+++ babel.config.js
@@ -4,37 +4,56 @@ module.exports = {
         ['@babel/preset-env', { useBuiltIns: 'entry', corejs: 3 }]
     ],
     plugins: [
-        // 'transform-proto-to-assign',
-        // 'transform-decorators-legacy',
-        // 'transform-runtime',
+        // Plugins sorted by stage
+        // Checkout https://github.com/tc39/proposals for proposal status.
+        // Checkout https://babeljs.io/docs/en/plugins-list for available plugins.
 
-        // Stage 0
+        // needs consensus
+        // https://github.com/tc39/proposal-export-ns-from
+        // '@babel/plugin-proposal-export-namespace-from',
+
+        /**
+         * Stage 0
+         */
+        // bind operator: https://github.com/tc39/proposal-bind-operator
         '@babel/plugin-proposal-function-bind',
 
-        // Stage 1
+        /**
+         * Step 1
+         */
+        // export default from: https://github.com/tc39/proposal-export-default-from
         '@babel/plugin-proposal-export-default-from',
-        '@babel/plugin-proposal-logical-assignment-operators',
-        ['@babel/plugin-proposal-pipeline-operator', { 'proposal': 'minimal' }],
+        // do expressions: https://github.com/tc39/proposal-do-expressions
         '@babel/plugin-proposal-do-expressions',
+        // pipe operator: https://github.com/tc39/proposal-pipeline-operator
+        // ['@babel/plugin-proposal-pipeline-operator', { 'proposal': 'minimal' }],
 
-        // Stage 2
-        ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-        '@babel/plugin-proposal-function-sent',
-        '@babel/plugin-proposal-export-namespace-from',
+        /**
+         * Stage 2
+         */
+
+        // generator function.sent: https://github.com/tc39/proposal-function.sent
+        // '@babel/plugin-proposal-function-sent',
         '@babel/plugin-proposal-throw-expressions',
 
-        // Stage 3
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-syntax-import-meta',
-        ['@babel/plugin-proposal-class-properties', { 'loose': false }],
-        '@babel/plugin-proposal-json-strings',
+        /**
+         * Stage 3
+         */
+        // decorators
+        // https://github.com/tc39/proposal-decorators
+        ['@babel/plugin-proposal-decorators', { 'legacy': true }],
 
-        // Stage 4
-        '@babel/plugin-proposal-numeric-separator',
-        ['@babel/plugin-proposal-optional-chaining', { 'loose': false }],
-        ['@babel/plugin-proposal-nullish-coalescing-operator', { 'loose': false }],
+        // ['@babel/plugin-proposal-class-properties', { 'loose': false }],
+        // '@babel/plugin-proposal-json-strings',
+
+        /**
+         * Stage 4
+         */
+        // logical assignment
+        // https://github.com/tc39/proposal-logical-assignment
+        '@babel/plugin-proposal-logical-assignment-operators',
 
         // https://github.com/babel/babel/issues/9849
-        '@babel/plugin-transform-runtime'
+        '@babel/plugin-transform-runtime',
     ]
 };
