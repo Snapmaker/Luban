fb8857b4281fe4cbd359d0018a55b8581d2cb5e0
parachvte <ryannx6@gmail.com>
2023-01-18 21:20:50 +0800

    Fix: Fix display of right extruder temp. change

--- src/app/ui/widgets/Marlin/ParamsWrapper.jsx
+++ src/app/ui/widgets/Marlin/ParamsWrapper.jsx
@@ -5,12 +5,14 @@ import EditComponent from '../../components/Edit';
 
 
 const ParamsWrapper = (props) => {
+    const { editable = true } = props;
+
     return (
         <div className="sm-flex-overflow-visible margin-vertical-8 justify-space-between">
             <div className="height-32 width-176 display-inline text-overflow-ellipsis">{props.title}</div>
             <div className="sm-flex margin-left-24 overflow-visible">
                 {props.children}
-                <EditComponent {...props} handleSubmit={props.handleSubmit} initValue={props.initValue} />
+                <EditComponent {...props} handleSubmit={props.handleSubmit} initValue={props.initValue} disabled={!editable} />
             </div>
         </div>
     );
@@ -19,6 +21,7 @@ const ParamsWrapper = (props) => {
 ParamsWrapper.propTypes = {
     children: PropTypes.array,
     title: PropTypes.string,
+    editable: PropTypes.bool,
     handleSubmit: PropTypes.func.isRequired,
     initValue: PropTypes.number.isRequired,
 };
