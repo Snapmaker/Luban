d557283981715689735f0955c73b91893381083c
parachvte <ryannx6@gmail.com>
2023-02-03 16:25:46 +0800

    Refactor: Upgrade intro.js v4 -> v5 && Fix introjs.css import (#1912)
    * Refactor: Upgrade intro.js v4 -> v5 && Fix introjs.css import
    * Improvement: Allow custom photo quality

--- src/app/ui/pages/Laser.jsx
+++ src/app/ui/pages/Laser.jsx
@@ -1,5 +1,4 @@
 import { message } from 'antd';
-import 'intro.js/introjs.css';
 import path from 'path';
 import PropTypes from 'prop-types';
 import React, { useEffect, useRef, useState } from 'react';
