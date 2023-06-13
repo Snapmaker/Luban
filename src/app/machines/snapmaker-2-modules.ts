e772f09e0ef21d6a1f251f892d16ba3e98474f0e
parachvte <ryannx6@gmail.com>
2023-06-14 04:01:22 +0800

    Feature: Add option to enable/disable Quick Swap Kit (#2228)

--- src/app/machines/snapmaker-2-modules.ts
+++ src/app/machines/snapmaker-2-modules.ts
@@ -1,7 +1,17 @@
 import type { MachineModule } from '@snapmaker/luban-platform';
 
-export const QuickSwapKitModule: MachineModule = {
+export type QuickSwapKitModule = MachineModule & {
+    metadata: {
+        workRangeOffset?: number[];
+    };
+};
+
+export const quickSwapKitModule: QuickSwapKitModule = {
     identifier: 'snapmaker-2.0-quick-swap-module',
 
-    name: 'Snapmaker 2.0 Quick Swap Module',
+    name: 'Snapmaker 2.0 Quick Swap Kit',
+
+    metadata: {
+
+    }
 };
