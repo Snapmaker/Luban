a37e6139187f87380c6c683f4d5b5d73a7faeb9a
parachvte <ryannx6@gmail.com>
2023-06-16 20:53:29 +0800

    Fix: Fix missing bounding box initially

--- src/app/flux/printing/index.ts
+++ src/app/flux/printing/index.ts
@@ -562,6 +562,9 @@ export const actions = {
 
         // Create material on demand
         await dispatch(actions.ensurePresetModels());
+
+        dispatch(actions.applyProfileToAllModels());
+        dispatch(actions.updateBoundingBox());
     },
 
     /**
@@ -2013,6 +2016,7 @@ export const actions = {
         dispatch(actions.updateSavedPresetIds(PRINTING_MANAGER_TYPE_QUALITY, presetId, stackId));
         dispatch(actions.validateActiveQualityPreset(stackId));
         dispatch(actions.applyProfileToAllModels());
+        dispatch(actions.updateBoundingBox());
     },
 
     /**
