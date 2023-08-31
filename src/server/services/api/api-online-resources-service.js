24810550b2d74217f3c6b5508927e373fc6e6a13
IDKHTS <2811212491@qq.com>
2023-08-31 09:58:02 +0800

    Fix: Update online service server for test

--- src/server/services/api/api-online-resources-service.js
+++ src/server/services/api/api-online-resources-service.js
@@ -4,7 +4,8 @@ import logger from '../../lib/logger';
 
 let domain = 'http://localhost:8100';
 if (process.env.NODE_ENV === 'production') {
-    domain = 'https://api.snapmaker.com';
+    // domain = 'https://api.snapmaker.com';
+    domain = 'http://45.79.80.155:8100';
 }
 
 const log = logger('api:commands');
