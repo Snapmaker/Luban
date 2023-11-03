fe4d5ae0787fa1d40d05639a4e656bd1f3f56da7
parachvte <ryannx6@gmail.com>
2023-11-03 16:57:33 +0800

    Fix: Fix rotary module recognition

--- src/app/flux/workspace/index.ts
+++ src/app/flux/workspace/index.ts
@@ -486,6 +486,7 @@ export const actions = {
              *   { key: 7, moduleId: 1500, status: true },
              * ]
              */
+            // Only works for SM 2.0
             'machine:module-list': (options) => {
                 const moduleList = options.moduleList;
 
