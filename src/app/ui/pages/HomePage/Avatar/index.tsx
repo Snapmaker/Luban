ad9046b525cf8abe84ca6623d4ab5e4c1d3c1ac9
wanghu <2256896349@qq.com>
2024-10-15 12:43:30 +0800

    Feature: Change loagout hash

--- src/app/ui/pages/HomePage/Avatar/index.tsx
+++ src/app/ui/pages/HomePage/Avatar/index.tsx
@@ -12,7 +12,6 @@ const Avatar: React.FC = () => {
     const [loading, setLoading] = useState(false);
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [iframeUrl, setIframeUrl] = useState('');
-    const [modalTitle, setModalTitle] = useState('');
 
     const showModal = () => {
         setIsModalVisible(true);
@@ -21,11 +20,9 @@ const Avatar: React.FC = () => {
         setIsModalVisible(false);
     };
     const handleToken = async (token: string) => {
-        console.log('token24::: ', token);
         try {
             setLoading(true);
             const { body: res } = await api.getUserInfo(token);
-            console.log('res::: ', res);
             if (res.code === 200) {
                 setUserInfo(res.data);
             } if (res.code === 100010110) {
@@ -37,7 +34,6 @@ const Avatar: React.FC = () => {
             }
             setLoading(false);
         } catch (error) {
-            console.log('error::: ', error);
             log.info(`userinfo load error: ${error}`);
         }
     };
@@ -46,19 +42,16 @@ const Avatar: React.FC = () => {
         if (loading) return;
         setIframeUrl('https://id.snapmaker.com?postKey=Luban');
         showModal();
-        setModalTitle('Login');
     };
     // define Logout
     const handleLogout = () => {
         machineStore.set('machine-token', '');
         setUserInfo(null);
-        setIframeUrl('http://id.snapmaker.com/logout?postKey=Luban');
+        setIframeUrl('http://id.snapmaker.com/logout#Luban');
         showModal();
-        setModalTitle('Log out');
     };
     // define Main
     const handleMain = () => {
-        setModalTitle('官网');
         setIframeUrl('https://snapmaker.com');
         showModal();
     };
@@ -67,12 +60,9 @@ const Avatar: React.FC = () => {
         window.addEventListener(
             'message',
             (event) => {
-                console.log('event::: ', event);
                 setIframeUrl('https://snapmaker.com');
                 const token = event.data;
-                console.log('token 52::: ', token);
                 if (token && typeof token === 'string') {
-                    console.log('token 54::: ', token);
                     if (token === 'logout') {
                         setIframeUrl('https://snapmaker.com');
                     } else {
@@ -84,16 +74,9 @@ const Avatar: React.FC = () => {
         );
         // get token from local storage
         const machineToken = machineStore.get('machine-token');
-        console.log('machineToken 61::: ', machineToken);
         if (machineToken) {
             handleToken(machineToken);
         }
-        // remove listener
-        return () => {
-            window.removeEventListener('message', (event) => {
-                console.log('event 73::: ', event);
-            });
-        };
     }, []);
 
     const menu = (
@@ -102,7 +85,7 @@ const Avatar: React.FC = () => {
                 <div className={styles.userInfoName}>{userInfo?.nickname}</div>
             </Menu.Item>
             <Menu.Item key="com" onClick={handleMain}>
-                官网
+                Snapmaker Website
             </Menu.Item>
             <Menu.Item key="logout" onClick={handleLogout}>
                 Logout
@@ -111,7 +94,7 @@ const Avatar: React.FC = () => {
     );
 
     return (
-        <div className={styles.avatarBox}>
+        <div id="avatar-box" className={styles.avatarBox}>
             {
                 userInfo?.id ? (
                     <Dropdown overlay={menu} trigger={['click', 'hover']}>
@@ -139,14 +122,14 @@ const Avatar: React.FC = () => {
             }
 
             <Modal
-                title={modalTitle}
                 open={isModalVisible}
                 onCancel={handleCancel}
                 className={styles.iframeModal}
                 footer={null}
+                getContainer={() => document.getElementById('avatar-box')}
             >
-                <div>
-                    <iframe id="dashboard" src={iframeUrl} title="ww" width="100%" height="600">
+                <div className={styles.iframeBox}>
+                    <iframe id="dashboard" key={iframeUrl} src={iframeUrl} title="iframe">
                         <p>Your browser does not support the iframe tag.</p>
                     </iframe>
                 </div>
